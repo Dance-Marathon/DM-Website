3b93be90b68d48b9f64769242495f41d4ca2d432
amigdall <amigdall@floridadm.org>
2015-01-21 17:36:03 -0500

    added staff app link to pay

--- staffapp.php
+++ staffapp.php
@@ -35,7 +35,7 @@
 			</div>
 				
 			<div class="col-md-8 col-md-push-1">
-				<p><h3>Step 1: Pay <a href="#">Here</a></h3></p>
+				<p><h3>Step 1: Pay <a href="http://floridadm.kintera.org/staffapp" target="_blank">Here</a></h3></p>
 				<p><h3>Step 2: Apply Below</h3></p>
 				<p><b>If you're on a mobile device, please submit your application via this <a href="http://goo.gl/forms/2Zes8f8Qpj" target="_blank">Link</a>.</b></p>
   				<p align="center"><iframe src="https://docs.google.com/forms/d/1gBm0RvGDyBDnqSznASyWumDADGwCTNgz_Go1E3sFZfo/viewform?embedded=true" width="760" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></p>
