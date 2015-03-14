ca8cb9d192147f60c63eaef92ab6a83ecea4a08c
Eric Leite <ericxleite@gmail.com>
2015-03-14 12:48:20 -0400

    date update

--- js/custom.js
+++ js/custom.js
@@ -65,7 +65,7 @@
 			
 			// Homepage DM countdown
       var currentDate = new Date();
-			var futureDate  = new Date("March 16, 2015 14:15:00");
+			var futureDate  = new Date("March 15, 2015 14:15:00");
 			var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 			jQuery('#countdown').FlipClock(diff, {
 			  clockFace: 'DailyCounter',
