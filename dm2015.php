5328287f9bcbdc13c2a72280a44a351f62d2e8fa
Eric Leite <ericxleite@gmail.com>
2015-03-14 20:46:30 -0400

    automated event timeline

--- dm2015.php
+++ dm2015.php
@@ -27,164 +27,153 @@
           <iframe class="embed-responsive-item" src="//www.youtube.com/v/lt_iXSfnxJc"></iframe>
         </div>
   		</div>
-			<div class="col-md-12">
+			<div class="col-md-12" id="data-replace">
   			<h1 style="margin: 66px auto 40px; text-align: center">Event Timeline</h1>
-
+  			<div id="loading"></div>
+        <div role="tabpanel">
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs" id="load-nav" role="tablist">
+            <li role="presentation" class="active"><a href="#all" aria-controls="all" role="tab" data-toggle="tab">All</a></li>
+          </ul>
           <!-- Tab panes -->
-              <table class="table table-bordered" style="margin-bottom: 0">
-                <thead>
-                  <tr>
-                  	<th></th>
-                    <th>Entertainment</th>
-                    <th>Family</th>
-                    <th>General</th>
-                    <th>Hospitality</th>
-                    <th>Linedance</th>
-                    <th>Special</th>
-                    <th>Theme Hour</th>
-                  </tr>
-                </thead>
-          		<tbody>
-                	<tr>
-                    <tr>
-                    	<td class="col-sm-2">11:00 AM - 12:00 PM</td>
-                        <td></td>
-                        <td></td>
-                        <td>Spirit Point Check-In 1<br/>Opening Ceremonies</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">12:00 PM - 1:00 PM</td>
-                        <td></td>
-                        <td>Family Lunch</td>
-                        <td>Spirit Point Check-Out 1</td>
-                        <td></td>
-                        <td>Teach Linedance Pt. 1</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">1:00 PM - 2:00 PM</td>
-                        <td>Peter Wrabbit</td>
-                        <td>Family Lunch</td>
-                        <td></td>
-                        <td>Meal 1</td>
-                        <td>Teach Linedance Pt. 2</td>
-                        <td>Jailbreak</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">2:00 PM - 3:00 PM</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>Jailbreak</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">3:00 PM - 4:00 PM</td>
-                        <td></td>
-                        <td>Owen M. & Geoffrey P. (3:00-3:10 PM)<br/>Bailey W. & Ayden M. (3:40-3:50 PM)</td>
-                        <td>Spirit Point Check-In 2</td>
-                        <td>Snack 2</td>
-                        <td>Linedance</td>
-                        <td>Jailbreak<br/>CMN Group Picture</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">4:00 PM - 5:00 PM</td>
-                        <td></td>
-                        <td>Kendall L. & Avery T. (4:40-4:50 PM)</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>Jailbreak<br/>2014 OT Linedance</td>
-                        <td>Dr. Seuss</td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">5:00 PM - 6:00 PM</td>
-                        <td></td>
-                        <td>Sage P. & Danny P. (5:40-5:50 PM)</td>
-                        <td>Spirit Point Check-In 3</td>
-                        <td>Meal 2</td>
-                        <td></td>
-                        <td>Jailbreak</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">6:00 PM - 7:00 PM</td>
-                        <td>Duke and the Groove Crew</td>
-                        <td></td>
-                        <td>Spirit Point Check-Out 3</td>
-                        <td>Meal 2</td>
-                        <td></td>
-                        <td>Jailbreak</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">7:00 PM - 8:00 PM</td>
-                        <td>Zumba</td>
-                        <td>William C. & Miranda L. (7:40-7:50 PM)</td>
-                        <td></td>
-                        <td></td>
-                        <td>Linedance</td>
-                        <td>Jailbreak</td>
-                        <td>ESPN</td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">8:00 PM - 9:00 PM</td>
-                        <td>Latchkey</td>
-                        <td></td>
-                        <td>Silent Auction<br/>Spirit Point Check-In 4</td>
-                        <td>Meal 3</td>
-                        <td></td>
-                        <td>Jailbreak</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">9:00 PM - 10:00 PM</td>
-                        <td></td>
-                        <td>Alison J. & Garrett L. (9:10-9:20 PM)</td>
-                        <td>Bounce House and Mood Room Open</td>
-                        <td>Meal 3/Snack 3</td>
-                        <td>Linedance</td>
-                        <td>Jailbreak</td>
-                        <td>Toy Story</td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">10:00 PM - 11:00 PM</td>
-                        <td>Dance Off</td>
-                        <td>Kalee E. (10:50-11:00 PM)</td>
-                        <td></td>
-                        <td></td>
-                        <td>Linedance</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                    	<td class="col-sm-2">11:00 PM - 12:00 AM</td>
-                        <td>Grit</td>
-                        <td></td>
-                        <td>Floor & Visitor Area Close</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>America</td>
-                    </tr>
-                </tbody>
-              </table>
+          <div class="tab-content" id="load-content">
+          </div>
+        </div>
 		  </div>
 			<div class="col-md-12">
         <h1 style="margin: 66px auto 40px; text-align: center">Floor Map</h1>
         <div id="floor-map" style="width: 50%; margin: 0 auto;"><img src="assets/images/odome.png"></div>
 			</div>
+		</div>
 	</div>
 </div>
-</div>
+
+<script src="lib/jquery-dateFormat.min.js"></script>
+<script type="text/javascript">  
+jQuery(document).ready(function() { 
+  var timeNow = new Date();
+  var url = "http://dev.floridadm.org/app/events.php";
+	var events;
+  $("html,body").promise().done(function(){
+    $("#data-replace").css({ opacity: 0.5 });
+    $("#loading").show();
+    $.ajax({
+    	url: url,
+    	data: $(this).serialize(),
+    	dataType: "json"
+    }).done(function(data) {
+    	$("#data-replace").animate({opacity: 0}, 200);
+    	$("#loading").hide();
+    	$("#data-replace").promise().done(function(){
+      	var tabCategories = [], uniqueTabCategories = [], uniqueTabCategoriesEncoded = [], tabCategoriesHTML = [], tabContent = [], tabTableHTML = [], relevantData = [], allData = [], dataObj, firstTabContent;
+        
+        // build categories array
+        for (var i = 0; i < data.length; i++) {
+          dataObj = data[i];
+          tabCategories.push(dataObj.category);
+        }
+        
+        // filter out repeated values
+        $.each(tabCategories, function(i, el){
+          if($.inArray(el, uniqueTabCategories) === -1) uniqueTabCategories.push(el);
+        });
+        
+        // sort alphabetically
+        uniqueTabCategories = uniqueTabCategories.sort();
+        
+        // write first tab content
+        var k = 0;
+        for (var j = 0; j < data.length; j++) {
+          allData[k] = new Array();
+          dataObj = data[j];
+          
+          a = dataObj.endDate.split(' ');
+          b = a[0].split('-');
+          c = a[1].split(':');
+          
+          var timeEnd = new Date(b[0], b[1]-1, b[2], c[0], c[1], c[2]);
+          
+          // save the data for the matching event
+          if (timeEnd > timeNow) {
+            allData[k].push(dataObj.title, dataObj.category, dataObj.startDate, dataObj.endDate);
+            k++;
+          }
+        }
+        
+        // build tab content HTML
+        firstTabContent = '<div role="tabpanel" class="tab-pane fade in active" id="all"><table class="table table-bordered"><thead><tr><th class="col-xs-4">Time</th><th class="col-xs-8">Event</th></tr></thead><tbody>';
+        
+        for (var j = 0; j < allData.length; j++) {
+          var formattedStart = $.format.date(allData[j][2], "h:mm a");
+          var formattedEnd = $.format.date(allData[j][3], "h:mm a");
+          firstTabContent += '<tr><td>'+formattedStart+' - '+formattedEnd+'</td><td>'+allData[j][0]+'</td></tr>';
+        }
+        
+        firstTabContent += '</tbody></table></div>';
+
+        // output tab content HTML
+        $("#data-replace #load-content").append(firstTabContent);
+        
+        // loop through each tab category
+        for (var i = 0; i < uniqueTabCategories.length; i++) {
+          
+          // encode categories for anchor usage
+          uniqueTabCategoriesEncoded[i] = uniqueTabCategories[i].replace(/\s+/g, '-').toLowerCase();
+          
+          // build tab category HTML
+          tabCategoriesHTML[i] = '<li role="presentation"><a href="#'+uniqueTabCategoriesEncoded[i]+'" aria-controls="'+uniqueTabCategoriesEncoded[i]+'" role="tab" data-toggle="tab">'+uniqueTabCategories[i]+'</a></li>';
+          
+          // output tab categories HTML
+          $("#data-replace #load-nav").append(tabCategoriesHTML[i]);
+          
+          relevantData[i] = new Array();
+
+          // loop through each event
+          var k = 0;
+          for (var j = 0; j < data.length; j++) {
+            dataObj = data[j];
+            
+            a = dataObj.endDate.split(' ');
+            b = a[0].split('-');
+            c = a[1].split(':');
+            
+            var timeEnd = new Date(b[0], b[1]-1, b[2], c[0], c[1], c[2]);
+            
+            // check if the current event category matches the current tab category and if the event is over
+            if (dataObj.category == uniqueTabCategories[i]) {
+              if (timeEnd > timeNow) {
+                relevantData[i][k] = new Array();
+                // save the data for the matching event
+                relevantData[i][k].push(dataObj.title, dataObj.category, dataObj.startDate, dataObj.endDate);
+                k++;
+              }
+            }
+          
+          }
+          
+          // build tab content HTML
+          tabContent[i] = '<div role="tabpanel" class="tab-pane fade" id="'+uniqueTabCategoriesEncoded[i]+'"><table class="table table-bordered"><thead><tr><th class="col-xs-4">Time</th><th class="col-xs-8">Event</th></tr></thead><tbody>'
+          
+          tabTableHTML[i] = "";
+          
+          for (var k = 0; k < relevantData[i].length; k++) {
+            var formattedStart = $.format.date(relevantData[i][k][2], "h:mm a");
+            var formattedEnd = $.format.date(relevantData[i][k][3], "h:mm a");
+            tabTableHTML[i] += '<tr><td>'+formattedStart+' - '+formattedEnd+'</td><td>'+relevantData[i][k][0]+'</td></tr>';
+          }
+          
+          tabContent[i] += tabTableHTML[i]+'</tbody></table></div>';
+
+          // output tab content HTML
+          $("#data-replace #load-content").append(tabContent[i]);
+        }
+        
+    		$("#data-replace").animate({opacity: 1}, 200);
+    	});
+    	
+    });
+  });
+});
+</script>
 
 <?php include("includes/foot.php"); ?>
