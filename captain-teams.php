eab966bbd1a4c717ddc412594475ef7afe595576
KimBro <kkimbroughjr@hotmail.com>
2015-08-23 07:01:23 -0400

    Updated Captain Teams Page
    removed old teams and descriptions and added new ones

--- captain-teams.php
+++ captain-teams.php
@@ -40,95 +40,94 @@
           <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="q1">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="false" aria-controls="a1">Art & Layout</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="false" aria-controls="a1">Community Events</a>
               </h4>
             </div>
             <div id="a1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q1">
-              <div class="panel-body">
-                <p>Responsible for designing any and all graphics needed for Dance Marathon, including but not limited to fliers, pamphlets, social media designs, advertisements and event-related graphics. Also responsible for maintaining the cohesive look of the Dance Marathon at UF brand via an official branding guide to ensure that it is both recognizable and sustainable in all design and graphics. Applicants should be familiar with Adobe InDesign, Illustrator and/or Photoshop.</p>
-                <p>This team is also responsible for all of the event photography both during the year and at the event. These applicants should possess their own cameras and ideally be familiar with shooting manual on a DSLR. Photoshop knowledge is not required, but preferred.</p>
-              </div>
-            </div>
-          </div>
-          <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q2">
-              <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a2" aria-expanded="false" aria-controls="a2">Community Events</a>
-              </h4>
-            </div>
-            <div id="a2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q2">
               <div class="panel-body">
                 Community Events Captains are responsible for organizing and participating in the events leading up to the 26.2 hour Dance Marathon. The highly anticipated events encourage the Gainesville and University of Florida Community to give back to the Kids. Events held prior to Dance Marathon may include a bowling tournament, Kickin' it For the Kids kickball tournament, FT5K race, Family Day at the Zoo and the Dance Marathon kick-off BBQ. Additionally, Community Events Captains organize activities at Dance Marathon including Jailbreak.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q4">
+            <div class="panel-heading" role="tab" id="q2">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a4" aria-expanded="false" aria-controls="a4">Dancer Relations</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a2" aria-expanded="false" aria-controls="a2">Creative Development</a>
               </h4>
             </div>
-            <div id="a4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q4">
+            <div id="a2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q2">
+              <div class="panel-body">
+                The Creative Development Captains are responsible for the Dance Marathon website, mobile applications on iOS and Android. This team is also in charge of all graphic design needs (e.g. social media campaigns outsourced through Facebook events and cover photos, Instagram graphics, website graphics, t-shirt proofs, newsletters, calendars, etc.) throughout the year. The Creative Development Captains also manage at-event content shown on the jumbotrons, such as Dancer Walk information, meal slides, and the livestream.              
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="q3">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#a3" aria-expanded="false" aria-controls="a3">Dancer Relations</a>
+              </h4>
+            </div>
+            <div id="a3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q3">
               <div class="panel-body">
                 Responsible for taking care of the dancers from the beginning of the year to the event’s end. Responsibilities include registering dancers for the actual event, tallying the spirit point competition that takes place throughout the year at various check-ins, holding informational dancer meetings, staffing the medical and study rooms at the event, handling guest check-ins at the event for dancer's visitors and maintaining communication with registered organizations throughout the year to handle any questions and concerns. Dancer Relations also coordinates an in-depth delegate program ensuring dancers and organizations have updated and accurate information about all upcoming events, deadlines and Dance Marathon as a whole.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q5">
+            <div class="panel-heading" role="tab" id="q4">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a5" aria-expanded="false" aria-controls="a5">Entertainment</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a4" aria-expanded="false" aria-controls="a4">Entertainment</a>
               </h4>
             </div>
-            <div id="a5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q5">
+            <div id="a4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q4">
               <div class="panel-body">
                 Entertainment Captains are responsible for booking entertainment for Dance Marathon, such as the DJ and organizing cameo presentations for the 26.2 hour event. As a team, Entertainment also provides music and performers at the events leading up to Dance Marathon, hosts and organizes a benefit concert and a talent show.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q6">
+            <div class="panel-heading" role="tab" id="q5">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a6" aria-expanded="false" aria-controls="a6">Family Relations</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a5" aria-expanded="false" aria-controls="a5">Family Relations</a>
               </h4>
             </div>
-            <div id="a6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q6">
+            <div id="a5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q5">
               <div class="panel-body">
                 Family Relations Captains serve as the liaison between Dance Marathon and the Miracle Families. These responsibilities include contacting Miracle Families, organizing the pen pal program, soliciting hotel rooms, coordinating events with the Miracle Families and organizations and organizing the Family Day event in the fall. As a team, Family Relations also decorates the family room, organizes the Miracle Family talent show and keeps the Miracle Families entertained during the 26.2 hour event.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q7">
+            <div class="panel-heading" role="tab" id="q6">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a7" aria-expanded="false" aria-controls="a7">Finance</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a6" aria-expanded="false" aria-controls="a6">Finance</a>
               </h4>
             </div>
-            <div id="a7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q7">
+            <div id="a6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q6">
               <div class="panel-body">
                 As a member of the Finance Team, you will work in close association with the Finance Overall Director on a variety of responsibilities, including financial oversight, funds processing, collections, and fundraising strategies. The finance team plays an integral role in the success of Dance Marathon at UF by ensuring that our total reveal is accurate up until the very last minute when the numbers are raised. The Finance team is also responsible for running dancer registration along with the Dancer Relations team. Alongside this, every Finance Captain will be responsible for all financial oversight for a handful of organizations. This oversight will require close relationships with both the delegates of those organizations and the corresponding Dancer Relations Captain.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q8">
+            <div class="panel-heading" role="tab" id="q7">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a8" aria-expanded="false" aria-controls="a8">Hospitality</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a7" aria-expanded="false" aria-controls="a7">Hospitality</a>
               </h4>
             </div>
-            <div id="a8" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q8">
+            <div id="a7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q7">
               <div class="panel-body">
                 Responsible for planning, soliciting, and serving a continuous and consistent flow of snacks and meals to the dancers including those with special dietary needs so as to maintain their motivation, stamina, and nutritional well being throughout the event. Also responsible for providing food for all community events prior to Dance Marathon and organizing benefit nights at local restaurants.
               </div>
             </div>
           </div>
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="q9">
+            <div class="panel-heading" role="tab" id="q8">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a9" aria-expanded="false" aria-controls="a9">Marathon Relations</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a8" aria-expanded="false" aria-controls="a8">Marathon Relations</a>
               </h4>
             </div>
-            <div id="a9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q9">
+            <div id="a8" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q8">
               <div class="panel-body">
                 <p>Responsible for advising and attending Dance Marathon at UF’s high school marathons. Also responsible for energizing and supporting these marathons by teaching the line dance and educating students on Children’s Miracle Network Hospitals and DM at UF’s history and culture. Focuses on strengthening relationships with high schools around the state while helping them to plan and put on their marathons, as well as recruiting new high schools to join the dance marathon family. Additionally, Marathon Relations maintains relationships with other collegiate Dance Marathons by serving as a resource and hosting other collegiate dance marathons.</p>
                 <p>Other responsibilities include fostering a relationship with and overseeing communications and outreach with UF faculty and staff.</p>
@@ -136,26 +135,38 @@
             </div>
           </div>
           <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="q9">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#a9" aria-expanded="false" aria-controls="a9">Merchandise</a>
+              </h4>
+            </div>
+            <div id="a9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q9">
+              <div class="panel-body">
+                The Merchandise Team is responsible for all Dance Marathon at the University of Florida merchandise and stores, in addition to running the Silent Auction at event. This team is also responsible for creating an official product offering for DM at UF apparel and accessories, running the DM Store in Turlington every Wednesday and at community events, facilitating pre-orders with Delegates and organizations, taking inventory, and organizing check-in shirt and event shirt orders. The Merchandise Team is also responsible for the ordering of Dancer Bag items as well as stuffing the bags. Merchandise Captains will help the Sponsorship team facilitate Marketing Days. The Merchandise team will also manage the online store and fulfill orders.              
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="q10">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a10" aria-expanded="false" aria-controls="a10">Marketing</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a10" aria-expanded="false" aria-controls="a10">Morale</a>
               </h4>
             </div>
             <div id="a10" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q10">
               <div class="panel-body">
-                Responsible for designing a marketing scheme for Dance Marathon, soliciting the community and national businesses for monetary and in-kind donations and facilitating and providing the benefits of the Dancer Packages. Marketing organizes and runs the Dance Marathon Silent Auction during the event and sells all DM merchandise.
+                Being a part of the Morale Team includes many important responsibilities. These are creating, planning and executing "theme hours" which are essentially themed hours filled with games, activities and crafts to keep the dancers entertained and enthused. Morale is also responsible for keeping the spirit of Dance Marathon high, energetic and positive throughout the entire event and the entire year prior to the 26.2 hour marathon. Morale is also the design team behind the revered linedance, which inspires and connects everyone at the event. Morale is an esteemed, dynamic and integral part of the Dance Marathon team!
               </div>
             </div>
           </div>
           <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="q11">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a11" aria-expanded="false" aria-controls="a11">Morale</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a11" aria-expanded="false" aria-controls="a11">Multimedia</a>
               </h4>
             </div>
             <div id="a11" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q11">
               <div class="panel-body">
-                Being a part of the Morale Team includes many important responsibilities. These are creating, planning and executing "theme hours" which are essentially themed hours filled with games, activities and crafts to keep the dancers entertained and enthused. Morale is also responsible for keeping the spirit of Dance Marathon high, energetic and positive throughout the entire event and the entire year prior to the 26.2 hour marathon. Morale is also the design team behind the revered linedance, which inspires and connects everyone at the event. Morale is an esteemed, dynamic and integral part of the Dance Marathon team!
+                The Multimedia Team is responsible for videography and throughout the year and the event. The team works to edit, sort and upload videos and photos regularly to YouTube and Facebook, respectively. The Multimedia Team is responsible for the acquisition of equipment for all filming and photography including all software involved for editing videos and photos. The Multimedia Team also works closely with the Creative Development Team to select pictures needed for graphics and other photographic needs. This team will be responsible for the Dance Marathon YouTube channel, as well as the Live Stream and Live Feed at the Event. It is recommended that Captains have experience with the Adobe Suite and/or other editing software.              
               </div>
             </div>
           </div>
@@ -198,15 +209,12 @@
           <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="q15">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordion" href="#a15" aria-expanded="false" aria-controls="a15">Technology</a>
+                <a data-toggle="collapse" data-parent="#accordion" href="#a15" aria-expanded="false" aria-controls="a15">Sponsorship</a>
               </h4>
             </div>
             <div id="a15" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q15">
               <div class="panel-body">
-                <p>The Dance Marathon Technology Team is divided into two distinct divisions: the Programming Division and the Video Production division.</p>
-                <p>The Programming Division is responsible for producing and maintaining custom applications designed to benefit Dance Marathon and its participants. This includes our custom iOS and Android applications, along with several PHP based web-apps. Experience in PHP, Objective-C, Java, and jQuery are useful for this position, although any programming background is sufficient.</p>
-                <p>The Video Production Division is responsible for producing all of our promotional videos and videos for the event itself. Experience in Adobe Premiere is preferred, although Final Cut is also sufficient.</p>
-                <p>At the event itself the programming division handles the audio/video production for the event along with maintaining any custom software that is in use. The video division handles documenting the event and producing our closing ceremonies videos.</p>
+                The Sponsorship Team is responsible for soliciting the national and local community for sponsorships ranging from title sponsorships, to in-kind donations for community events and Dance Marathon, to items for the Silent Auction. They are also responsible for fostering stewardship with sponsors through increased communication and the creation of "Sponsor Portfolios," which will show sponsors all benefits received throughout the year.              
               </div>
             </div>
           </div>
