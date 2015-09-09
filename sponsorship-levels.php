ed3e496fd5d3a4c71af212bd976ee40e8efeab63
KimBro <kkimbroughjr@hotmail.com>
2015-09-09 10:56:21 -0400

    Updated Sponsorship Levels Page
    This page now simply embeds the PDF with sponsorship levels in each tab

--- sponsorship-levels.php
+++ sponsorship-levels.php
@@ -44,121 +44,14 @@
             </div>
             <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
               <div class="panel-body">
-                <ul class="sponsor">
-                  <li class="level-1">
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Premium placed logo on DM at UF website homepage scrolling banner</li>
-                          <li>Opportunity to table on floor for 2 hours at event</li>
-                          <li>Opportunity to name an award presented on stage at closing ceremonies</li>
-                          <li>Opportunity to present donation check on stage of event during sponsor hour</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Miracle</h4>
-                        <span>$15,001+</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Oppurtunity to table at event during sponsor hour</li>
-                          <li>Opportunity to hang two banners at event</li>
-                          <li>Personal “Thank You” video for your social media use</li>
-                          <li>Featured in miracle child “Thank You” video during sponsor hour at event</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Champion</h4>
-                        <span>$10,001-$15,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company logo on official DM at UF Event shirt</li>
-                          <li>30 second commercial during sponsor hour</li>
-                          <li>Full page advertisement in event program</li>
-                          <li>Opportunity to table at a community event</li>
-                          <li>Facebook “Thank You” photo post and link</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>FTK</h4>
-                        <span>$5,001-$10,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company name on event t-shirt</li>
-                          <li>Recognition in collective post-event thank you</li>
-                          <li>Upgraded to two coupons in dancer bags</li>
-                          <li>Opportunity to make “sponsor spotlight” video to be played at event</li>
-                          <li>Custom recognition plaque</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Gator</h4>
-                        <span>$2,501-$5,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Opportunity to hang one banner at event</li>
-                          <li>Quarter page advertisement in event program</li>
-                          <li>Logo included on scrolling display at event</li>
-                          <li>Company logo in Alumni Newsletter sent to DM at UF Alumni nationally</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Family</h4>
-                        <span>$1,501-$2,500</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company logo displayed on DM at UF website</li>
-                          <li>VIP Corporate Partner tour at the event</li>
-                          <li>Coupon in over 800 dancer bags</li>
-                          <li>Recognition in official event program </li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Hope</h4>
-                        <span>$501-<br>$1,500</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company Name and link listed on DM at UF Website</li>
-                          <li>Twitter Sponsor Shout Out</li>
-                          <li>Proud Supporter of Dance Marathon at UF Decal</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Friendship</h4>
-                        <span>$250-<br>$500</span>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
+                <object data="assets/PDFs/SponsorshipLevelsM.pdf" type="application/pdf" width="100%" height="600px">
+                 
+                  <p>It appears you don't have a PDF plugin for this browser.
+                  No biggie... you can <a href="assets/PDFs/SponsorshipLevelsM.pdf">click here to
+                  download the PDF file.</a></p>
+
+                </object>
+                
               </div>
             </div>
           </div>
@@ -174,122 +67,17 @@
             </div>
             <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
               <div class="panel-body">
-                <ul class="sponsor">
-                  <li class="level-1">
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Premium placement of logo on event shirt</li>
-                          <li>Opportunity to table on floor for 2 hours at event</li>
-                          <li>30 second commercial during sponsor hour</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Miracle</h4>
-                        <span>$20,001+</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Opportunity to table at event during sponsor hour</li>
-                          <li>Premium placed logo on DM at UF website homepage scrolling banner</li>
-                          <li>Featured in miracle child “Thank You” video during sponsor hour at event</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Champion</h4>
-                        <span>$15,001-$20,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company logo on official DM at UF Event shirt</li>
-                          <li>Opportunity to table at a community event</li>
-                          <li>Facebook “Thank You” photo post and link</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>FTK</h4>
-                        <span>$10,001-$15,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Receive a custom recognition plaque</li>
-                          <li>Opportunity to submit a 5-10 second "business spotlight and clip of support" to be played at event<sup>*</sup></li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Gator</h4>
-                        <span>$5,001-$10,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Company name on event t-shirt</li>
-                          <li>Company logo on DM at UF website</li>
-                          <li>Quarter page advertisement in event program</li>
-                          <li>Logo included on scrolling display at event</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Family</h4>
-                        <span>$2,001-$5,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel right">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Twitter shout out and link</li>
-                          <li>Company logo and link on DM at UF website</li>
-                          <li>Coupon in over 800 dancer bags</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Hope</h4>
-                        <span>$601-<br>$2,000</span>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="sponsor-panel left">
-                      <div class="sponsor-content">
-                        <ul>
-                          <li>Proud Supporter of Dance Marathon at UF Decal</li>
-                          <li>Sponsor Name listed on DM at UF Website</li>
-                        </ul>
-                      </div>
-                      <div class="sponsor-circle">
-                        <h4>Friendship</h4>
-                        <span>$0 -<br>$600</span>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
+                <object data="assets/PDFs/SponsorshipLevelsIK.pdf" type="application/pdf" width="100%" height="600px">
+                 
+                  <p>It appears you don't have a PDF plugin for this browser.
+                  No biggie... you can <a href="assets/PDFs/SponsorshipLevelsIK.pdf">click here to
+                  download the PDF file.</a></p>
+
+                </object>
               </div>
             </div>
           </div>
         </div>
-
-        <p><sup>*</sup>Provided by Sponsor</p>
-        <ul>
-          <li>Monetary donations are counted at face value</li>
-          <li>In-Kind donations are counted at 1/2 face value for Partnership Level purposes</li>
-        </ul>
 
       <?php } else { echo 'This page is currently closed.'; } ?>
       </div>
