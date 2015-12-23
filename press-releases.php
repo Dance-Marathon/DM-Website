6d67d75f0e752e08ea1113a7b9c595df943c6c47
KimBro <kkimbroughjr@hotmail.com>
2015-12-22 19:13:32 -0500

    Added Transform Today to this years Press Releases

--- press-releases.php
+++ press-releases.php
@@ -24,10 +24,10 @@
 			<div class="col-md-3">
   			<div class="sub-nav">
           <ul>
-						<li><a href="/nomenclature.php">Nomenclature</a></li>
-						<li><a class="active">Press Releases</a></li>
-						<li><a href="/press-contacts.php">Press Contacts</a></li>
-						<li><a href="/fast-facts.php">Fast Facts</a></li>
+			<li><a href="/nomenclature.php">Nomenclature</a></li>
+			<li><a class="active">Press Releases</a></li>
+			<li><a href="/press-contacts.php">Press Contacts</a></li>
+			<li><a href="/fast-facts.php">Fast Facts</a></li>
           </ul>
         </div>
 			</div>
@@ -38,10 +38,15 @@
           <li><a href="/Documents/DM2015PressRelease.pdf" target="_blank">Dance Marathon 2015</a> 03/07/15</li>
           <li><a href="/Documents/FTKanrival2015PressRelease.pdf" target="_blank">FTKarnival</a> 02/24/2015</li>
           <li><a href="/Documents/FT5K-Press-Release-2015.pdf" target="_blank">FT5K</a> 02/08/2015</li>
-          <li><a href="/Documents/Transform-Today-Press-Release.pdf" target="_blank">Transform Today</a> 01/08/15</li>
+          <li><a href="/Documents/TransformTodayPressRelease2014.pdf" target="_blank">Transform Today</a> 01/08/15</li>
           <li><a href="/Documents/CYMplify-Food-Truck-Rally-Press-Release.pdf" target="_blank">CYMplify Food Truck Rally</a> 12/05/2014</li>
           <li><a href="/Documents/DMKickball2014.pdf" target="_blank">DM Kickball</a> 10/16/2014</li>
         </ul>
+        <h3>2015-2016 Press Releases</h3>
+        <ul>
+          <li><a href="/Documents/TransformTodayPressRelease2015.pdf" target="_blank">Transform Today</a> 11/18/15</li>
+        </ul>
+
         <p>If you have any questions, please contact Armani Abreu, Public Relations Overall at <a href="mailto:aabreu@floridadm.org">aabreu@floridadm.org</a>.</p>
       <?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
