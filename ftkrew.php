b27994b647e9225a5c35a40f24d191089bc38129
KimBro <kkimbroughjr@hotmail.com>
2016-01-10 20:51:33 -0500

    Added list of FTKrew

--- ftkrew.php
+++ ftkrew.php
@@ -36,17 +36,617 @@
 			</div>
 			<div class="col-md-8 col-md-push-1">
       <?php if ($status == "open") { ?>
-      	<h3>FTKrew Applications</h3>
-        <p>Out with the old and in with the new.  We have listened to your feedback about Staff and created a new position that is more involved and invested in Dance Marathon at the University of Florida, called FTKrew.  There are two different FTKrew teams, <b>Morale FTKrew</b> and <b>Marathon FTKrew</b>.  Morale FTKrew will be working with a specific theme hour throughout the event and assisting Morale Captains with their tasks. Marathon FTKrew will work with the Dancer Relations, Operations and Hospitality team captains to help with their responsibilities throughout the event. </p> <p> As a member of the FTKrew, you will be asked to attend meetings throughout the spring semester, attend all Dance Marathon at UF events and fundraise $300.  Interviews will be held, <b>January 4th-8th from 5-10pm</b>.  A doodle will be emailed to you after applications close.  Selections will be made after interviews. </p>
-		<ul>
-			<li><a href="https://docs.google.com/forms/d/1HXzJYHb9R9zI3S_JnXqTpGrgo8FBkVlANq32xcWEseE/viewform?usp=send_form" target="_blank">2016 FTKrew Application</a></li>
-			<li><a href="https://events.dancemarathon.com/event/ftkrew" target="_blank">Payment Link</a></li>
-		</ul>
-        <p>We are excited to offer these new teams and to extend our Dance Marathon family! If you have any questions, please contact Jenna Baxter at <a href="mailto:jbaxter@floridadm.org">jbaxter@floridadm.org</a>.</p>
-      <?php } else { ?>
-        <p class="alert alert-info"><b>Note:</b> FTKrew Applications are closed.</p>
-        <p>Please direct questions to Jenna Baxter, current Internal Communications Overall Director at <a href="mailto:jbaxter@floridadm.org">jbaxter@floridadm.org</a>.</p>
-      <?php } ?>
+      	<h3>FTKrew</h3>
+        <p>Out with the old and in with the new.  We have listened to your feedback about Staff and created a new position that is more involved and invested in Dance Marathon at the University of Florida, called FTKrew.  There are two different FTKrew teams, <b>Morale FTKrew</b> and <b>Marathon FTKrew</b>.  Morale FTKrew will be working with a specific theme hour throughout the event and assisting Morale Captains with their tasks. Marathon FTKrew will work with the Dancer Relations, Operations and Hospitality team captains to help with their responsibilities throughout the event. </p> <p> Members of FTKrew attend meetings throughout the spring semester, attend all Dance Marathon at UF events and fundraise $300. </p>
+        <h3>Marathon FTKrew</h3>
+				<table style="width:100%">
+					<tr>
+						<td>Abbi Clack</td>
+						<td>Abby Gears</td>
+						<td>Abigail Danos</td>
+					</tr>
+					<tr>
+						<td>Aldo Colon</td>
+						<td>Alejandra Dumenigo</td>
+						<td>Alejandra Llano</td>
+					</tr>
+					<tr>
+						<td>Alexa Julia</td>
+						<td>Alexa Ruiz</td>
+						<td>Alexandra Csortan</td>
+					</tr>
+					<tr>
+						<td>Alexia Barrios</td>
+						<td>Alexia Schmidt</td>
+						<td>Alyssa Skryd</td>
+					</tr>
+					<tr>
+						<td>Amanda Abes</td>
+						<td>Amanda Devitt</td>
+						<td>Amanda Guixens</td>
+					</tr>
+					<tr>
+						<td>Andi Anchell</td>
+						<td>Andrew Jensen</td>
+						<td>Andrew Kelly</td>
+					</tr>
+					<tr>
+						<td>Anthony Davila</td>
+						<td>Ariel Harris</td>
+						<td>Avery Nelson</td>
+					</tr>
+					<tr>
+						<td>Avery Zaffos</td>
+						<td>Bailey Burrows</td>
+						<td>Brandon Mackenzie</td>
+					</tr>
+					<tr>
+						<td>Brian Betancourt</td>
+						<td>Brian Spivak</td>
+						<td>Brooke Garrison</td>
+					</tr>
+					<tr>
+						<td>Brooke Rabinovich</td>
+						<td>Carly Mackler</td>
+						<td>Caroline Baker</td>
+					</tr>
+					<tr>
+						<td>Casey Sargent</td>
+						<td>Catherine Lower</td>
+						<td>Charlie friedfeld</td>
+					</tr>
+					<tr>
+						<td>Chelsea Bourekas</td>
+						<td>Chelsea Carrasquillo </td>
+						<td>Chelsea Katz</td>
+					</tr>
+					<tr>
+						<td>Chloe Burgess</td>
+						<td>Chris Bott</td>
+						<td>Christian Chambers</td>
+					</tr>
+					<tr>
+						<td>Connor Miller</td>
+						<td>Courtney Maras</td>
+						<td>Courtney Mikush</td>
+					</tr>
+					<tr>
+						<td>Courtney Smithgall</td>
+						<td>Dani LeGrand</td>
+						<td>Dani Ruadez</td>
+					</tr>
+					<tr>
+						<td>Dani Vogel</td>
+						<td>Dani Young</td>
+						<td>Danielle Maso</td>
+					</tr>
+					<tr>
+						<td>Danielle Randle</td>
+						<td>Danielle Vargas</td>
+						<td>Denise Cabrera</td>
+					</tr>
+					<tr>
+						<td>Devyn Nathan</td>
+						<td>Elaine williams</td>
+						<td>Elena Moore</td>
+					</tr>
+					<tr>
+						<td>Elizabeth Mahe</td>
+						<td>Emani Green</td>
+						<td>Emily Crumit</td>
+					</tr>
+					<tr>
+						<td>Emma LaBarbera</td>
+						<td>Emma Rutherford</td>
+						<td>Gabriela Morales</td>
+					</tr>
+					<tr>
+						<td>Gabriella Valentin</td>
+						<td>Gabrielle Solovay</td>
+						<td>Garrett crawford</td>
+					</tr>
+					<tr>
+						<td>Genevieve Horton</td>
+						<td>Grant Wishner</td>
+						<td>Henry Edmondson</td>
+					</tr>
+					<tr>
+						<td>Hope Kennedy</td>
+						<td>Jackie Coleman</td>
+						<td>Jacob Scheulder</td>
+					</tr>
+					<tr>
+						<td>Jacquelin Sheehan</td>
+						<td>Jade Bartlett</td>
+						<td>Jake Faber</td>
+					</tr>
+					<tr>
+						<td>James Vessley</td>
+						<td>Jan Strusinski</td>
+						<td>Jared Blinderman</td>
+					</tr>
+					<tr>
+						<td>Jennifer Rodiguez </td>
+						<td>Jessica Liu</td>
+						<td>Jessica Spear</td>
+					</tr>
+					<tr>
+						<td>Jocelyn Gordon</td>
+						<td>Joel Buenrostro</td>
+						<td>John Ferraro</td>
+					</tr>
+					<tr>
+						<td>John Mctighe</td>
+						<td>Jonathan Nickas</td>
+						<td>Jordan Isbitts</td>
+					</tr>
+					<tr>
+						<td>Jordan Weber</td>
+						<td>Jordana Roth </td>
+						<td>Joshua Benrubi</td>
+					</tr>
+					<tr>
+						<td>Julia McMongile</td>
+						<td>Julia Sabra</td>
+						<td>Julie Giurintano</td>
+					</tr>
+					<tr>
+						<td>Justin Burt</td>
+						<td>Justin Weidenfeld</td>
+						<td>Kaitlyn Wendling</td>
+					</tr>
+					<tr>
+						<td>Kaley Knapp</td>
+						<td>Karina Martinez</td>
+						<td>Kassandra Philogene</td>
+					</tr>
+					<tr>
+						<td>Kathryn Merwin</td>
+						<td>Katrina Ramos</td>
+						<td>Kayla Hennie</td>
+					</tr>
+					<tr>
+						<td>Kaylin Ellison</td>
+						<td>Kelly Venturi</td>
+						<td>Kelsey Bridges</td>
+					</tr>
+					<tr>
+						<td>Kim Novak</td>
+						<td>Kristin Wentz</td>
+						<td>Kyle Stodard</td>
+					</tr>
+					<tr>
+						<td>Kylee Sipowski</td>
+						<td>Kylie Paul</td>
+						<td>Lauren Dubose</td>
+					</tr>
+					<tr>
+						<td>Lauren Feininger</td>
+						<td>Lauren Hausman</td>
+						<td>Lauren Lockwood</td>
+					</tr>
+					<tr>
+						<td>Lexie Miller</td>
+						<td>Lindsay Roth</td>
+						<td>Lindsey Nacht</td>
+					</tr>
+					<tr>
+						<td>Lindsey Watkins</td>
+						<td>Lisa Marinelli</td>
+						<td>Lizabeth Lohmann</td>
+					</tr>
+					<tr>
+						<td>Logan Matura</td>
+						<td>Lora Hargett</td>
+						<td>Madison Anthiby</td>
+					</tr>
+					<tr>
+						<td>Mandi Gaynor</td>
+						<td>Mariel Rotbart</td>
+						<td>Marissa Kappus</td>
+					</tr>
+					<tr>
+						<td>Marni Weiss</td>
+						<td>Megan Hazlett</td>
+						<td>Megan Somerville</td>
+					</tr>
+					<tr>
+						<td>Meredith Sheldon</td>
+						<td>Meredith Wiwant</td>
+						<td>Michael Orndorf</td>
+					</tr>
+					<tr>
+						<td>Mikhail Cunill</td>
+						<td>Molly Cuthbertson</td>
+						<td>Molly Secrest</td>
+					</tr>
+					<tr>
+						<td>Monica Ionescu</td>
+						<td>Morgan Berger</td>
+						<td>Nadia Khoja</td>
+					</tr>
+					<tr>
+						<td>Natalie Crespo</td>
+						<td>Natalie Fernandez</td>
+						<td>Nena Guzman</td>
+					</tr>
+					<tr>
+						<td>Nicole Donato</td>
+						<td>Nikki Kosco</td>
+						<td>Nina Cusmano</td>
+					</tr>
+					<tr>
+						<td>Rachel Elliott</td>
+						<td>Rachel Sharpe</td>
+						<td>Rachel Soloman</td>
+					</tr>
+					<tr>
+						<td>Rachel Wiener</td>
+						<td>Randi Ziegler</td>
+						<td>Reed Brinkman</td>
+					</tr>
+					<tr>
+						<td>Roi Epstein</td>
+						<td>Ross Hughes</td>
+						<td>Ryan Schoel</td>
+					</tr>
+					<tr>
+						<td>Sam Williams</td>
+						<td>Samantha Kitroser</td>
+						<td>Samantha Matott</td>
+					</tr>
+					<tr>
+						<td>Samantha Uanino</td>
+						<td>Sanchita Mukherjee</td>
+						<td>Sarah Ciampi</td>
+					</tr>
+					<tr>
+						<td>Sarah DeLeon</td>
+						<td>Sarah Spardy</td>
+						<td>Sarah Vititoe</td>
+					</tr>
+					<tr>
+						<td>Savannah Oglesby</td>
+						<td>Seth Shanefield</td>
+						<td>Shahreen Shahreen</td>
+					</tr>
+					<tr>
+						<td>Shaun Civin</td>
+						<td>Simon Pavlovic </td>
+						<td>Sofia Restrepo</td>
+					</tr>
+					<tr>
+						<td>Sophia Anagnostis</td>
+						<td>Stephanie Kouvasto</td>
+						<td>Suzy Dean</td>
+					</tr>
+					<tr>
+						<td>Sydney Seif</td>
+						<td>Talia Akerman</td>
+						<td>Tara Segalewatz</td>
+					</tr>
+					<tr>
+						<td>Taylor Clark</td>
+						<td>Taylor Hupp</td>
+						<td>Taylor Rose</td>
+					</tr>
+					<tr>
+						<td>Taylor Roth</td>
+						<td>Taylor Stone</td>
+						<td>Teal Garth</td>
+					</tr>
+					<tr>
+						<td>Trey Fritz</td>
+						<td>Troy Arcand</td>
+						<td>Truc Le</td>
+					</tr>
+					<tr>
+						<td>Victoria Brown</td>
+						<td>Vincent Castellano</td>
+						<td>Zachary Kravetz</td>
+					</tr>
+      	</table>
+      	<h3>Morale FTKrew</h3>
+				<table style="width:100%">
+					<tr>
+						<td>Aaron Graubert</td>
+						<td>Abigail Guthrie</td>
+						<td>Abigail Wilwant</td>
+					</tr>
+					<tr>
+						<td>Alex Woeste</td>
+						<td>Alexa Engel </td>
+						<td>Alexa Steinlauf</td>
+					</tr>
+					<tr>
+						<td>Alexandra Eaton</td>
+						<td>Alexandra Notricia</td>
+						<td>Ali Bowlby</td>
+					</tr>
+					<tr>
+						<td>Alison Catena</td>
+						<td>Alivia Perrone</td>
+						<td>Allison Denehy</td>
+					</tr>
+					<tr>
+						<td>Allison Goldberg</td>
+						<td>Allison Luntz</td>
+						<td>Allison Rudolph</td>
+					</tr>
+					<tr>
+						<td>Allison Sherman </td>
+						<td>Alyssa Wolff</td>
+						<td>Amanda Russo</td>
+					</tr>
+					<tr>
+						<td>Amanda Rutner</td>
+						<td>Amanda Sarbey</td>
+						<td>Ana Saavedra</td>
+					</tr>
+					<tr>
+						<td>Anastassia Kerasidis</td>
+						<td>Andie Tradler</td>
+						<td>Andrea Benaim</td>
+					</tr>
+					<tr>
+						<td>Angie Reinhart</td>
+						<td>Anna Pirovic</td>
+						<td>Anna Villagomez</td>
+					</tr>
+					<tr>
+						<td>Ashley Brooks</td>
+						<td>Ashley De Tullio</td>
+						<td>Ashley Jacobs</td>
+					</tr>
+					<tr>
+						<td>Ashton Putnal</td>
+						<td>Aubrey Rooney</td>
+						<td>Brittney Hayes</td>
+					</tr>
+					<tr>
+						<td>Caitlin Allweiss</td>
+						<td>Caitlin Perry</td>
+						<td>Carly Girnun</td>
+					</tr>
+					<tr>
+						<td>Caroline Rollosson</td>
+						<td>Cassidy Long</td>
+						<td>Catherine Gannon</td>
+					</tr>
+					<tr>
+						<td>Catherine Haley</td>
+						<td>Christie Goldstein</td>
+						<td>Claire Cardillo</td>
+					</tr>
+					<tr>
+						<td>Corinne Sharabi</td>
+						<td>Courtney Newell</td>
+						<td>Courtney Steinberg</td>
+					</tr>
+					<tr>
+						<td>Dakota Stanford</td>
+						<td>Dana Congress</td>
+						<td>Dani Josephson</td>
+					</tr>
+					<tr>
+						<td>Deanice Cruz</td>
+						<td>Delaney Jardine</td>
+						<td>Delilah Ponce</td>
+					</tr>
+					<tr>
+						<td>dominique ketsogio</td>
+						<td>Dorothy Hellmann</td>
+						<td>Elizabeth Lamm</td>
+					</tr>
+					<tr>
+						<td>Emily Prins</td>
+						<td>Emily Taylor</td>
+						<td>Emily Washier</td>
+					</tr>
+					<tr>
+						<td>Emma Coleman</td>
+						<td>Emma Hamilton</td>
+						<td>Emma Popowski</td>
+					</tr>
+					<tr>
+						<td>Erica Camacho</td>
+						<td>Erica Steinkohl</td>
+						<td>Francia Baez</td>
+					</tr>
+					<tr>
+						<td>Gabby Marton</td>
+						<td>Gabriel Rojas </td>
+						<td>Gaby Meitin</td>
+					</tr>
+					<tr>
+						<td>Gianna Kornetti</td>
+						<td>Hailey Robles-Holm</td>
+						<td>Haley Aouate</td>
+					</tr>
+					<tr>
+						<td>Haley Wathen</td>
+						<td>Hannah Kneeland</td>
+						<td>Hannah Medina </td>
+					</tr>
+					<tr>
+						<td>Hannah Phillips</td>
+						<td>heather padgett</td>
+						<td>Helen Jeong</td>
+					</tr>
+					<tr>
+						<td>Holly Finney</td>
+						<td>Hunter Hopkins</td>
+						<td>Ilyssa Schatz</td>
+					</tr>
+					<tr>
+						<td>Jack Yao</td>
+						<td>James Antoine</td>
+						<td>Jamie Somerstein</td>
+					</tr>
+					<tr>
+						<td>Janine Legaspi</td>
+						<td>Jenna Litt</td>
+						<td>Jenna Mukerjee</td>
+					</tr>
+					<tr>
+						<td>Jordan Paul</td>
+						<td>Joseph Tarnate</td>
+						<td>Josh Canter</td>
+					</tr>
+					<tr>
+						<td>Julia Renner</td>
+						<td>Justin Morfe</td>
+						<td>Kailee Vickaryous</td>
+					</tr>
+					<tr>
+						<td>Kailey Kynast</td>
+						<td>Kaitlyn Cheshire</td>
+						<td>Kaitlyn Mairs</td>
+					</tr>
+					<tr>
+						<td>Kaleigh Owen</td>
+						<td>Kassidy Schaeffer</td>
+						<td>Katelyn Cornelius</td>
+					</tr>
+					<tr>
+						<td>Kathryn Barry</td>
+						<td>Katrina Moya</td>
+						<td>Kayla Kolka</td>
+					</tr>
+					<tr>
+						<td>Kelly Ann Kennedy</td>
+						<td>Kelsey Abbey</td>
+						<td>Kelsey Congress</td>
+					</tr>
+					<tr>
+						<td>Kevin Rivera</td>
+						<td>Kristina Rojas</td>
+						<td>Krupa Patel</td>
+					</tr>
+					<tr>
+						<td>Lauren Bruss</td>
+						<td>Lauren Palen</td>
+						<td>Lauren White</td>
+					</tr>
+					<tr>
+						<td>Leigh Bouskila</td>
+						<td>Lillie Shipley</td>
+						<td>Lindsay Beveridge</td>
+					</tr>
+					<tr>
+						<td>Lindsey Harrison</td>
+						<td>Lizzie Ayers</td>
+						<td>Logan Peck</td>
+					</tr>
+					<tr>
+						<td>Lyndsay Goldberg</td>
+						<td>Maddie Harlow </td>
+						<td>Maija Sunnarborg</td>
+					</tr>
+					<tr>
+						<td>Marisa Gottlieb</td>
+						<td>Marisa Masters</td>
+						<td>Marni Rochkind </td>
+					</tr>
+					<tr>
+						<td>Max Barry</td>
+						<td>Max Sibner</td>
+						<td>McKenna Krause</td>
+					</tr>
+					<tr>
+						<td>McKenna Ville</td>
+						<td>Megan Hall</td>
+						<td>Megan Schroder</td>
+					</tr>
+					<tr>
+						<td>Melanie Fletcher</td>
+						<td>Melissa Clauss</td>
+						<td>Melissa Miranda</td>
+					</tr>
+					<tr>
+						<td>Meredith Schoppe</td>
+						<td>Michael Backstrand</td>
+						<td>Michelle Maldonado</td>
+					</tr>
+					<tr>
+						<td>Michelle Montoya</td>
+						<td>Mikaela Balzer</td>
+						<td>Molly Vossler</td>
+					</tr>
+					<tr>
+						<td>Monica Andrade</td>
+						<td>Morgan Coffey</td>
+						<td>Morgan Schober</td>
+					</tr>
+					<tr>
+						<td>Natalia Veray</td>
+						<td>Natalie Martinez</td>
+						<td>Natalie Vala</td>
+					</tr>
+					<tr>
+						<td>Natasha Jewett</td>
+						<td>Nicole Fitos</td>
+						<td>Patricia Wojtowicz</td>
+					</tr>
+					<tr>
+						<td>Paxton Shamlou</td>
+						<td>Payton Ledman</td>
+						<td>Payton Strader</td>
+					</tr>
+					<tr>
+						<td>Rachel Chozick</td>
+						<td>Rachel Knauf</td>
+						<td>Rachel Kohl</td>
+					</tr>
+					<tr>
+						<td>Rachel Rockwell</td>
+						<td>Rachel Roth</td>
+						<td>Rachel Weinsoff </td>
+					</tr>
+					<tr>
+						<td>Reese Mischler</td>
+						<td>Riley Mullins</td>
+						<td>Ruth Harrington </td>
+					</tr>
+					<tr>
+						<td>Sarah Lazarus</td>
+						<td>Savannah Broome</td>
+						<td>Savannah Harding</td>
+					</tr>
+					<tr>
+						<td>Savannah Nelson</td>
+						<td>Savannah Nuttall</td>
+						<td>shannon martin</td>
+					</tr>
+					<tr>
+						<td>Shelly Trak</td>
+						<td>Sofia Dayi</td>
+						<td>Stephanie Hersman</td>
+					</tr>
+					<tr>
+						<td>Sullivan Henry</td>
+						<td>Suzannah Boyle</td>
+						<td>Sydney Lieberman</td>
+					</tr>
+					<tr>
+						<td>Tabitha Bickman</td>
+						<td>Taylor Bakalar </td>
+						<td>Taylor Katz</td>
+					</tr>
+					<tr>
+						<td>Taylor Lambertus</td>
+						<td>Thalia Rey</td>
+						<td>Tracy Love</td>
+					</tr>
+					<tr>
+						<td>Trinity  Turinetti</td>
+						<td>Valerie Berman</td>
+						<td>Vicoria Vincent</td>
+					</tr>
+					<tr>
+						<td>Zachary Feldman</td>
+					</tr>
+				</table>
       </div>
 		</div>
 	</div>
