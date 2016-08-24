db9d181f4c1218ffc1d6a0b8dae2143578ae22e1
tmoreno93 <tamimoreno10@gmail.com>
2016-08-24 19:16:41 -0400

    Added FAQ to ELP page

--- emerging-leaders.php
+++ emerging-leaders.php
@@ -34,10 +34,11 @@ include("includes/navbar.php");
 						<li><a class="active">Emerging Leaders Program</a></li>
 						<li><a href="/applications">Captain & ELP Applications</a></li>
 
-					</ul>
+				</ul>
 				</div>
 			</div>
 			<div class="col-md-8 col-md-push-1">
+				<div>
 				<h3>Program Description</h3>
 				<p>
 					The purpose of the Dance Marathon Emerging Leaders Program is to cultivate the skills of emerging leaders at the University of Florida within the Dance Marathon program. The aim of the program is to engage students who have a passion to help others and who believe in our vision of curing childhood illness. The Dance Marathon Emerging Leaders Program is a new and unique first step for students desiring to join the Dance Marathon movement.
@@ -45,9 +46,109 @@ include("includes/navbar.php");
 				<p>
 					Being a member of the Dance Marathon Emerging Leaders Program means attending monthly general body meetings as well as weekly small group meetings. At general body meetings, members will have the opportunity to learn more about the goals and efforts of Dance Marathon, and the opportunity to hear from Miracle Families and various speakers from across the university. In the small group meetings, members will become part of a group of around twenty students who will help in making UF a new home away from home for each other. Each week will present members new ways to contribute to and improve Dance Marathon at UF while growing as individuals within the organization. We will work with our members to develop their leadership skills and provide them with opportunities to make a difference in the world, beginning with making a world of difference in the life of a child.
 				</p>
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingOne">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+								Where does the Emerging Leaders Program fall in Dance Marathon’s structure?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+						<div class="panel-body">
+							The Emerging Leaders Program is separate from anything that Dance Marathon has done in the past. The Emerging Leaders program will be overseen by the Recruitment Team.						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingTwo">
+						<h4 class="panel-title">
+							<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Is the program only for freshmen?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+						<div class="panel-body">
+							Absolutely not! The program is for students that are new to UF, new to Dance Marathon, or are just looking for a way to get involved on campus while developing their leadership skills within Dance Marathon.						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingThree">
+						<h4 class="panel-title">
+							<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								Are there any qualifications to join?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+						<div class="panel-body">
+							Just a positive attitude and an eagerness to join us in working toward our vision of ending childhood illness.
+						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingFour">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+								Can I apply to be a Captain as well as an Emerging Leader?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+						<div class="panel-body">
+							Yes! Just complete the Captain application and indicate that you’d like to be considered for both programs. You will be prompted for your preference. Best part is - you only need to pay one application fee!
+						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingFive">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+								Is there an application fee?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+						<div class="panel-body">
+							Yes - $25. Note: If you choose to apply to a Captain Team AND the Emerging Leaders Program, you only need to pay one application fee ($25 total).
+						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingSix">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+								What is the time commitment?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+						<div class="panel-body">
+							There will be weekly meetings that are the bare minimum to stay involved in the program. There will also be opportunities to attend Dance Marathon events throughout the year, and we definitely encourage you to do so!						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingSeven">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+								What is the program going to look like?
+							</a>
+						</h4>
+					</div>
+					<div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+						<div class="panel-body">
+							Once accepted to the program, you will be placed into a small group of about 20 students. Each group will be lead by 2-3 Recruitment Captains, and the Captains will be responsible for teaching their group leadership skills as well as engaging them in Dance Marathon activities and events.
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
 	</div>
+</div>
+
+
+
+
 </div>
 
 <?php include("includes/foot.php"); ?>
