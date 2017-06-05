6739ab5fd601216d19c4536105a0eb64b621a586
sregula <samregula@gmail.com>
2017-06-05 14:45:43 -0400

    ft5k header

--- ft5k.php
+++ ft5k.php
@@ -6,7 +6,7 @@
 	include("includes/navbar.php");
 ?>
 
-<div class="page-heading parallax ft5k">
+<div class="page-heading parallax community_events">
   <div class="inner-wrapper">
 	  <div class="container">
     	<div class="row">
