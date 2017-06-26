6ba5ab781d5370736abc01942df9f7868b3736d6
sregula <samregula@gmail.com>
2017-06-26 08:30:37 -0400

    opened help pre-app page

--- applications.php
+++ applications.php
@@ -1,5 +1,5 @@
 <?php
-	$status = "closed";
+	$status = "open";
 	$GLOBALS['page_title'] = 'Captain & ELP Applications | Get Involved | Florida Dance Marathon';
 	$GLOBALS['parent'] = 'get-involved';
 	include("includes/head.php");
