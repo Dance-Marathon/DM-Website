832efdd8eca0d04b3f4b4ada12fd08f590fb089e
Jonathan Pereyra <jonathanpereyra@me.com>
2018-01-20 23:15:01 -0500

    udpated head.php

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
         <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicons/apple-touch-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/favicons/apple-touch-icon-114x114.png">
         <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/favicons/apple-touch-icon-72x72.png">
@@ -24,43 +24,41 @@
         <meta name="msapplication-TileImage" content="/assets/images/favicons/mstile-144x144.png">
         <meta name="msapplication-config" content="/assets/images/favicons/browserconfig.xml">
 
-		<title><?php echo $page_title ?></title>
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <title><?php echo $page_title ?></title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
         <!-- injector:css -->
-        <link rel="stylesheet" href="/dist/1513132805939/css/DM-Website.min.css">
         <!-- endinjector -->
 
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		  <script src="/ie/html5shiv.min.js"></script>
-		  <script src="/ie/respond.min.js"></script>
-		<![endif]-->
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="/ie/html5shiv.min.js"></script>
+          <script src="/ie/respond.min.js"></script>
+        <![endif]-->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
     <!-- injector:js -->
-    <script src="/dist/1513132805939/js/DM-Website.min.js"></script>
     <!-- endinjector -->
 
     <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-31255631-5', 'auto');
-	  ga('send', 'pageview');
-	</script>
-	</head>
+      ga('create', 'UA-31255631-5', 'auto');
+      ga('send', 'pageview');
+    </script>
+    </head>
 
-	<?php
+    <?php
 
-	$dm_year = "2017";
-	$dance_marathon_event_started = false;
+    $dm_year = "2017";
+    $dance_marathon_event_started = false;
 
-	?>
+    ?>
 
-	<body>
+    <body>
