4db51115e1cac128f2c475e9675a99296ce1cdb6
gruszczykr <gruszczykr@ufl.edu>
2018-05-28 18:28:49 -0400

    showgoeson Compressed image

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       </ol> -->
       <div class="carousel-inner">
         <div class="item active">
-          <img class="img-responsive"  src="assets/images/slider/showgoeson.png">
+          <img class="img-responsive"  src="assets/images/slider/theshowgoeson.png">
           <div class="carousel-caption text-center">
             <div class="row animate_fade_in">
               <!-- <div class="col-md-6">
