601659cc97006a1bb29e97d870d81efbf62f5c9e
Gruszczykr <gruszczykr@gmail.com>
2018-09-08 12:27:29 -0400

    ELPs

--- emerging-leaders.php
+++ emerging-leaders.php
@@ -7,62 +7,432 @@ include("includes/navbar.php");
 ?>
 
 <div class="page-heading parallax elp">
-	<div class="inner-wrapper">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h1>Emerging Leaders Program</h1>
-				</div>
-			</div>
-		</div>
-	</div>
+ <div class="inner-wrapper">
+	 <div class="container">
+		 <div class="row">
+			 <div class="col-md-12">
+				 <h1>Emerging Leaders Program</h1>
+			 </div>
+		 </div>
+	 </div>
+ </div>
 </div>
 
 <div class="page-content">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-3">
-				<div class="sub-nav">
-          <ul>
-						<li><a href="/alumni">Alumni</a></li>
-						<li><a href="/applications">Applications</a></li>
-						<li><a href="/captain-teams">Captain Teams</a></li>
-						<li><a href="/dancers">Dancers</a></li>
-						<li><a href="/ambassadors">Ambassadors</a></li>
-						<li><a class="active">Emerging Leaders</a></li>
-						<li><a href="/ftkrew">FTKrew</a></li>
-						<li><a href="/organizations">Organizations</a></li>
-          </ul>
-				</div>
-			</div>
-			<div class="col-md-8 col-md-push-1">
+ <div class="container">
+	 <div class="row">
+		 <div class="col-md-3">
+			 <div class="sub-nav">
+					<ul>
+					 <li><a href="/alumni">Alumni</a></li>
+					 <li><a href="/applications">Applications</a></li>
+					 <li><a href="/captain-teams">Captain Teams</a></li>
+					 <li><a href="/dancers">Dancers</a></li>
+					 <li><a href="/delegates">Delegates</a></li>
+					 <li><a class="active">Emerging Leaders</a></li>
+					 <li><a href="/ftkrew">FTKrew</a></li>
+					 <li><a href="/organizations">Organizations</a></li>
+					</ul>
+			 </div>
+		 </div>
+		 <div class="col-md-8 col-md-push-1">
+			 <div role="tabpanel">
+				 <!-- Nav tabs -->
+				 <ul class="nav nav-tabs" role="tablist">
+				 <li role="presentation" class="active"> <a href="#Program-Description" aria-controls="Program-Description" role="tab" data-toggle="tab"> ELP </a></li>
+			  <li role="presentation"> <a href="#Emerging-Leaders" aria-controls="Emerging-Leaders" role="tab" data-toggle="tab">Emerging Leaders</a></li>
+			<!-- <li role="presentation"> <a href="#FAQ" aria-controls="FAQ" role="tab" data-toggle="tab">FAQ</a></li> -->
+		 </ul>
+			 <div class="tab-content">
+				 <div role="tabpanel" class="tab-pane fade in active" id="Program-Description">
+			 <h3>Program Description</h3>
+<p></p>
+			 <!-- <p> <b><a href="https://ufl.qualtrics.com/jfe7/form/SV_e5nm2HHP9L8WzCR" target="_blank">ELP Pre-Applications</a></b> are open now! Pre-Applications will be open from <b>Monday, June 26th</b> through <b>Sunday, July 2nd</b> at 11:59 p.m, and won’t re-open until the Fall semester begins.</p> -->
+			 <p></p>
+				 <p>
+				 The purpose of the Dance Marathon Emerging Leaders Program is to cultivate the skills of emerging leaders at the University of Florida within the Dance Marathon organization. The aim of the program is to engage students who have a passion to help others and who believe in our vision of curing childhood illness. The Dance Marathon Emerging Leaders Program is a new and unique first step for students desiring to join the Dance Marathon movement.
+			 </p>
+			 <p>
+				 Being a member of the Dance Marathon Emerging Leaders Program means attending monthly general body meetings as well as weekly small group meetings. At general body meetings, members will have the opportunity to learn more about the goals and efforts of Dance Marathon, and the opportunity to hear from Miracle Families and various speakers from across the university. In the small group meetings, members will become part of a group of around twenty students who will help in making UF a new home away from home for each other. Each week will present members new ways to contribute to and improve Dance Marathon at UF while growing as individuals within the organization. We will work with our members to develop their leadership skills and provide them with opportunities to make a difference in the world, beginning with making a world of difference in the life of a child.
+			 </p>
+		 </div>
+	 <div role="tabpanel" class="tab-pane fade in active" id="Emerging-Leaders">
+						 <h3>2019 Emerging Leaders</h3>
+						 <p> Congratulations to our the members of 2019 Emerging Leaders Program! </p>
+						 <table style="width:100%">
+<tr>
+							 <td>	Abigail Boeneman	</td>
+							 <td>	Adam Rosenblum	</td>
+							 <td>	Adrian Mendoza	</td>
+							 <td>	Adriana Laine	</td>
+							 <td>	Aidan Evans	</td>
+							 <td>	Alex Goldman	</td>
+</tr>
+ <tr>
+	 <td>	Alexandra Guerra	</td>
+	 <td>	Alexandra Heyer	</td>
+	 <td>	alexia lugo	</td>
+	 <td>	Alivia Baxendell	</td>
+	 <td>	Allison Brummer	</td>
+	 <td>	Alyssa Morford	</td>
+</tr>
+<tr>
+	<td>	Amanda Fentress	</td>
+	<td>	Amanda Wichterman	</td>
+	<td>	Ana Ibarrola	</td>
+	<td>	Angelica Byrd	</td>
+	<td>	Annabelle Groux	</td>
+	<td>	Ansley Jones	</td>
+</tr>
+<tr>
+	<td>	Anthony Rahbany	</td>
+	<td>	Anya Wiener Benitez	</td>
+	<td>	Ariel Gordon	</td>
+	<td>	Ashia Hightower	</td>
+	<td>	Ashley Taylor	</td>
+	<td>	Ashton Greusel	</td>
+</tr>
+<tr>
+	<td>	Aubrey Kilduff	</td>
+	<td>	Ava Daly	</td>
+	<td>	Bailey Petty	</td>
+	<td>	Blakely Paymani	</td>
+	<td>	Breann Barone	</td>
+	<td>	Bridget Hartig	</td>
+</tr>
+<tr>
+	<td>	Brittany Steinfeld	</td>
+	<td>	Brooklyn Nave	</td>
+	<td>	Caden Cress	</td>
+	<td>	Caitlyn Pinto	</td>
+	<td>	Campbell Jones	</td>
+	<td>	Carli Sley	</td>
+</tr>
+<tr>
+	<td>	Carly Beverly	</td>
+  <td>	Carly Swanson	</td>
+  <td>	Cayla Epstein	</td>
+  <td>	Charles Hang	</td>
+  <td>	Chelsea Caplan	</td>
+  <td>	Chloe Judge	</td>
+</tr>
+<tr>
+	<td>	Chris Stallings	</td>
+	<td>	Clayton Gaulding	</td>
+	<td>	Colette Bialick	</td>
+	<td>	Collin Walsh	</td>
+	<td>	Corey Reeves	</td>
+	<td>	Coryell Dreyer	</td>
+</tr>
+<tr>
+	<td>	Courtney Crenshaw	</td>
+	<td>	Dalena Nguyen	</td>
+	<td>	Daniela Suarez	</td>
+	<td>	Danielle Silva	</td>
+	<td>	Daphne Furman	</td>
+	<td>	Dayanis Oliva	</td>
+</tr>
+<tr>
+	<td>	Deep PATEL	</td>
+  <td>	Delaney Czworka	</td>
+  <td>	Dina Modlin	</td>
+  <td>	Dominic Salamone	</td>
+  <td>	Dylan Nugent	</td>
+  <td>	Eliezer Cruz	</td>
+</tr>
+<tr>
+	<td>	Elisabeth (Lizzie) Lewalski	</td>
+  <td>	Emily Karas	</td>
+  <td>	Emily Myers	</td>
+  <td>	Eric De Rezende	</td>
+  <td>	Ethan Kramer	</td>
+  <td>	Faith Freeman	</td>
+</tr>
+<tr>
+	<td>	Francesca Ycaza	</td>
+	<td>	Gabriela Garcia	</td>
+	<td>	Gabriela Hernandez	</td>
+	<td>	Grace Hogan	</td>
+	<td>	Grace Tyler	</td>
+	<td>	Gracie Curran	</td>
+</tr>
+<tr>
+	<td>	Gregory Buettner	</td>
+	<td>	Haley Esperanza	</td>
+	<td>	Haley Spooner	</td>
+	<td>	Hannah Cohen	</td>
+	<td>	Hannah Howell	</td>
+	<td>	Hollyann Brown	</td>
+</tr>
+<tr>
+	<td>	Houston Barenholtz	</td>
+  <td>	Jack Braverman	</td>
+  <td>	Jack Brennan	</td>
+  <td>	Jack Kirsch	</td>
+  <td>	Jacob Warchola	</td>
+  <td>	Jake Peter	</td>
+</tr>
+<tr>
+	<td>	Jared Herbert	</td>
+	<td>	Jazlyn Edwards	</td>
+	<td>	Jenna Whitney	</td>
+	<td>	Jennifer Gilby	</td>
+	<td>	Jillian Hersman	</td>
+	<td>	Joel Rios-Rodriguez	</td>
+</tr>
+<tr>
+	<td>	John Hermes	</td>
+	<td>	Jonathan Fernandez	</td>
+	<td>	Jonathan Maya	</td>
+	<td>	Jonathan Trilla	</td>
+	<td>	Jonquil (Jonny) Sekula	</td>
+	<td>	Joseph Gage BonJorn	</td>
+</tr>
+<tr>
+	<td>	Joseph Lavoie	</td>
+	<td>	Julia Cordover	</td>
+	<td>	Juliana Licata	</td>
+	<td>	Juliana Tragakis	</td>
+	<td>	Kaiya-rai Sarmenta	</td>
+	<td>	Karis Perusek	</td>
+</tr>
+<tr>
+	<td>	Karli Kimbrough	</td>
+	<td>	Katherine Powell	</td>
+	<td>	Kathryn (Katie) Herstik	</td>
+	<td>	Kathryn Byers	</td>
+	<td>	Kathryn Quintero	</td>
+	<td>	Katie Puccio	</td>
+</tr>
+<tr>
+	<td>	Kayley Hamilton	</td>
+	<td>	Kelsy Coughlin	</td>
+	<td>	Kendall Tabachnik	</td>
+	<td>	Kendra Hall	</td>
+	<td>	Kira Pratico	</td>
+	<td>	Krista Stevens	</td>
+</tr>
+<tr>
+	<td>	Landry Baliton	</td>
+  <td>	Lauren Bouskila	</td>
+  <td>	Lauren Hetrzroni	</td>
+  <td>	Lauren Toledo	</td>
+  <td>	Leacy Burton	</td>
+  <td>	Leighton Scroggie	</td>
+</tr>
+<tr>
+	<td>	Lilli Levinson	</td>
+	<td>	Lilliam Drenth	</td>
+	<td>	Lily Mild	</td>
+	<td>	Luana Souza	</td>
+	<td>	Lucia Costas Velasco	</td>
+	<td>	Luis De La Rosa	</td>
+</tr>
+<tr>
+	<td>	Luz Mata	</td>
+  <td>	Lyndi George	</td>
+  <td>	Mackenzie Bender	</td>
+  <td>	Maddie Abdoney	</td>
+  <td>	Maddy Michielssen	</td>
+  <td>	Madison Artzt	</td>
+</tr>
+<tr>
+	<td>	Madison Camporese	</td>
+	<td>	Madison Duncanson	</td>
+	<td>	Madison Wnuk	</td>
+	<td>	mariana maciel	</td>
+	<td>	Mariana Monforte	</td>
+	<td>	Mark Pennington	</td>
+</tr>
+<tr>
+	<td>	Mary Kate Urbanski	</td>
+	<td>	Matias Villanueva	</td>
+	<td>	Matthew Comeau	</td>
+	<td>	Matthew Whitman	</td>
+	<td>	Matthew Wroblewski	</td>
+	<td>	Maxwell Gravenstine	</td>
+</tr>
+<tr>
+	<td>	Melissa Tempest	</td>
+  <td>	Michael Connelly	</td>
+  <td>	Michael Contu	</td>
+  <td>	Michael Sivilli	</td>
+  <td>	Michelle Fernandez-Valle	</td>
+  <td>	Miguelangel Ruiz	</td>
+</tr>
+<tr>
+	<td>	Molly Lantinberg	</td>
+  <td>	Morgan Hilton	</td>
+  <td>	Natalia Guerra	</td>
+  <td>	Natalia Thornton	</td>
+  <td>	Nawaf Alay	</td>
+  <td>	Nhuy Nguyen	</td>
+</tr>
+<tr>
+	<td>	Nicole Sanborn	</td>
+  <td>	Noah Denker	</td>
+  <td>	Olivia Panzica	</td>
+  <td>	Rachel Early	</td>
+  <td>	Rachel Friedman	</td>
+  <td>	Rachel Kelly	</td>
+</tr>
+<tr>
+	<td>	Rachel Pohl	</td>
+  <td>	Raegan Fink	</td>
+  <td>	Rajel Wolcoff	</td>
+  <td>	Ram Desai	</td>
+  <td>	Rebecca Allison	</td>
+  <td>	Rebecca Schlafke	</td>
+</tr>
+<tr>
+	<td>	Samuel Speizman	</td>
+  <td>	Sarah Blankenship	</td>
+  <td>	Sarah Garavaglia	</td>
+  <td>	Sarah Greep	</td>
+  <td>	Sarah Sheerer	</td>
+  <td>	Sarah Voskamp	</td>
+</tr>
+<tr>
+	<td>	Shannon Chamberlain	</td>
+	<td>	Shayna Cohen	</td>
+	<td>	Sheelagh Dunn	</td>
+	<td>	Skye Dougan	</td>
+	<td>	Sophie Charlson	</td>
+	<td>	Stephanie Kokeas	</td>
+</tr>
+<tr>
+	<td>	Susanna van de Graaf	</td>
+	<td>	Sydney Jimenez	</td>
+	<td>	Sydney Miller	</td>
+	<td>	Sydney Patterson	</td>
+	<td>	Sydnie Winder	</td>
+	<td>	Taryn Hibshman	</td>
+</tr>
+<tr>
+	<td>	Tej Patel	</td>
+	<td>	Thomas McGurk	</td>
+	<td>	Valentina Nunez	</td>
+	<td>	Valerie Jones	</td>
+	<td>	Victor Rivera	</td>
+	<td>	Vinny Savarino	</td>
+</tr>
+<tr>
+	<td>	Yasmine Gillespie	</td>
+  <td>	Zach Frielich	</td>
+  <td>	Zachary Hahn	</td>
+  <td>	Zachary Perez	</td>
+  <td>	Zayne Moore	</td>
+  <td>	Zoie Danyali	</td>
+</tr>
 
 
-				<div class="tab-content">
-				<h3>Program Description</h3>
-				<p></p>
-					<p>
-					The purpose of the Dance Marathon Emerging Leaders Program is to cultivate the skills of emerging leaders at the University of Florida within the Dance Marathon organization. The aim of the program is to engage students who have a passion to help others and who believe in our vision of curing childhood illness. The Dance Marathon Emerging Leaders Program is a new and unique first step for students desiring to join the Dance Marathon movement.
-				</p>
-				<p>
-					Being a member of the Dance Marathon Emerging Leaders Program means attending monthly general body meetings as well as weekly small group meetings. At general body meetings, members will have the opportunity to learn more about the goals and efforts of Dance Marathon, and the opportunity to hear from Miracle Families and various speakers from across the university. In the small group meetings, members will become part of a group of around twenty students who will help in making UF a new home away from home for each other. Each week will present members new ways to contribute to and improve Dance Marathon at UF while growing as individuals within the organization. We will work with our members to develop their leadership skills and provide them with opportunities to make a difference in the world, beginning with making a world of difference in the life of a child.
-				</p>
-			<h3>Emerging Leaders</h3>
-			<table style="width:100%">
-          <tr>    
-<td>  Alexia Yavalar  </td>
-<td>  Alexis Sutter </td>
-<td>  Bailey LaRea  </td>
-<td>  Alexia Yavalar  </td>
-<td>  Alexis Sutter </td>
-<td>  Bailey LaRea  </td>
-</tr>   
-	</table>
-			</div>
 
-					</div>
-				</div>
-  		</div>
+
+
+
+
+		 </div>
+							</table>
+					 </div>
+						 <!-- <div role="tabpanel" class="tab-pane fade" id="FAQ">
+						 <h3>FAQ</h3>
+						 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingOne">
+										 <h4 class="panel-title">
+											 <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+												 Where does the Emerging Leaders Program fall in Dance Marathon’s structure?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+										 <div class="panel-body">
+											 The Emerging Leaders Program is separate from anything that Dance Marathon has done in the past. The Emerging Leaders program will be overseen by the Recruitment Team.					</div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingTwo">
+										 <h4 class="panel-title">
+											 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+												 Is the program only for freshmen?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+										 <div class="panel-body">
+											 Absolutely not! The program is for students who are new to UF, new to Dance Marathon, or are just looking for a way to get involved on campus while developing their leadership skills within the Dance Marathon organization.					</div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingThree">
+										 <h4 class="panel-title">
+											 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+												 Are there any qualifications to join?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+										 <div class="panel-body">
+												 Just a positive attitude and an eagerness to join us in working towards our vision of ending childhood illness.						</div>
+									 </div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingFour">
+										 <h4 class="panel-title">
+											 <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+												 Can I apply to be a Captain as well as an Emerging Leader?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+										 <div class="panel-body">
+											 Yes! Just complete the Captain application and indicate that you’d like to be considered for both programs. You will be prompted for your preference. Best part is - you only need to pay one application fee!
+										 </div>
+									 </div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingFive">
+										 <h4 class="panel-title">
+											 <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+												 Is there an application fee?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+										 <div class="panel-body">
+											 Yes - $25. Note: If you choose to apply to a Captain Team AND the Emerging Leaders Program, you only need to pay one application fee ($25 total).
+										 </div>
+									 </div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingSix">
+										 <h4 class="panel-title">
+											 <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+												 What is the time commitment?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+										 <div class="panel-body">
+											 There will be weekly meetings that are the bare minimum to stay involved in the program. There will also be opportunities to attend Dance Marathon events throughout the year, and we definitely encourage you to do so!
+									 </div>
+								 </div>
+								 <div class="panel panel-default">
+									 <div class="panel-heading" role="tab" id="headingSeven">
+										 <h4 class="panel-title">
+											 <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+												 What is the program going to look like?
+											 </a>
+										 </h4>
+									 </div>
+									 <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+										 <div class="panel-body">
+												 Once accepted to the program, you will be placed into a small group of about 20 students. Each group will be lead by 2-3 Recruitment Captains, and the Captains will be responsible for teaching their groups leadership skills as well as engaging them in Dance Marathon activities and events. 						</div>
+											 </div>
+										 </div>
+									 </div>
+					 </div>-->
+				 </div>
+			 </div>
+		 </div>
 
 </div>
 </div>
