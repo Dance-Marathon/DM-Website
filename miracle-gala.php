d201563464096d7521f1c01ebc6767a19692abbc
Unknown <ctcook97@gmail.com>
2019-01-14 23:37:50 -0500

    Miracle Gala Update

--- miracle-gala.php
+++ miracle-gala.php
@@ -1,6 +1,6 @@
 <?php
 	$status = "open";
-	$GLOBALS['page_title'] = 'Gala | Events | Dance Marathon at UF';
+	$GLOBALS['page_title'] = 'Miracle Gala | Events | Dance Marathon at UF';
 	$GLOBALS['parent'] = 'events';
 	include("includes/head.php");
 	include("includes/navbar.php");
@@ -11,7 +11,7 @@
 	  <div class="container">
     	<div class="row">
 	  		<div class="col-md-12">
-    			<h1>Gala</h1>
+    			<h1>Miracle Gala</h1>
 	  		</div>
     	</div>
 	  </div>
@@ -35,11 +35,20 @@
 			<div class="col-md-8 col-md-push-1">
       <?php if ($status == "open") { ?>
 
-				<h2>Gala</h2>
+				<h2>Miracle Gala</h2>
 
-  			<p>
-  		Join us at the Touchdown Terrace on March 24, 2018 for the Miracle Gala sponsored by the University of Florida Department of Pediatrics. What better way to celebrate Dance Marathon at UF weekend than by enjoying an evening of fine dining, dancing, and recognizing all of the accomplishments of Dance Marathon at the University of Florida and the doctors at UF Health Shands Children’s Hospital? For more information, please contact the Outreach Overall Director, Grace Murray, at <a href="mailto:gmurray@floridadm.org">gmurray@floridadm.org</a>. We look forward to seeing you there!
-      <?php } else { echo 'This page is currently closed.'; } ?>
+				<p>What better way to celebrate Dance Marathon at UF weekend than by enjoying an evening of fine dining and celebrating our year long efforts to raise funds and awareness for UF Health Shands Children’s Hospital? This year’s Miracle Gala will take place on Saturday, March 30th, 2019 at the Holloway Touchdown Terrace at Ben Hill Griffin Stadium. The itinerary for the evening: </p>
+					<ul>
+						<li>6:00 PM: Cocktail Hour</li>
+						<li>6:00-7:30 PM: Optional tour of Main Event</li>
+						<li>8:00 PM: Dinner, Silent Auction, Raise the Paddle</li>
+					</ul>
+
+				<p>For more information, please contact the Community Outreach Overall Director, Grace Murray, at <a href = mailto:gmurray@floridadm.org>gmurray@floridadm.org</a>. Ticket sales and sponsorship opportunities found in our Sponsorship Packet <a href=https://drive.google.com/file/d/13Zo9qCGQePTCzvzRkCtkOgfnnBBEM2NN/view>here</a>. We look forward to seeing you there!</p>
+
+				<p><a href=https://squareup.com/market/dance-marathon-at-uf/item/miracle-gala>Tickets can be bought here</a></p>
+
+			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
 		</div>
 	</div>
