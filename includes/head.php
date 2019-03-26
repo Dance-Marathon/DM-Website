8251b666a3b5d44f95fcfc731e8860a2a90f32d6
Stephanie Nash <snash@ufl.edu>
2019-03-26 19:56:18 -0400

    ugh github

--- includes/head.php
+++ includes/head.php
@@ -29,7 +29,7 @@
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
         <!-- injector:css -->
-        <link rel="stylesheet" href="/dist/1552579011794/css/DM-Website.min.css">
+        <link rel="stylesheet" href="/dist/1552584213013/css/DM-Website.min.css">
         <!-- endinjector -->
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -42,7 +42,7 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
     <!-- injector:js -->
-    <script src="/dist/1552579011794/js/DM-Website.min.js"></script>
+    <script src="/dist/1552584213013/js/DM-Website.min.js"></script>
     <!-- endinjector -->
 
     <script>
@@ -50,17 +50,14 @@
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 	  ga('create', 'UA-31255631-5', 'auto');
 	  ga('send', 'pageview');
 	</script>
 	</head>
 
 	<?php
-
 	$dm_year = "2017";
 	$dance_marathon_event_started = false;
-
 	?>
 
 	<body>
