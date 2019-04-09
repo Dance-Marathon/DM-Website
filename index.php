fbde8d08ff857ed57340bf31a2336212df2624aa
Unknown <ctcook97@gmail.com>
2019-04-09 15:25:39 -0400

    Took livestream link off index and updated DMA link

--- index.php
+++ index.php
@@ -53,26 +53,6 @@
 
 <?php if ($dance_marathon_event_started == true) { ?>
 
-	 	<div id="event_banner">
-		<div class="row mt mb">
-			<div class="col-xs-12 welcome_txt text-center">
-				<h1 class="event_banner_title">
-					Dance Marathon <?php echo $dm_year; ?>
-					</h1>
-			</div>
-			<div class="col-xs-12 welcome_txt text-center">
-				<h1 class="event_banner_title">
-						<span style="color: #F79B63; background-color: white;">Is Here</span>
-					</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4 text-center">
-				<a id="event_page_button" class="btn hvr-underline-from-center" href="/dance-marathon">View Livestream</a>
-			</div>
-		</div>
-	</div>
-
 	<?php } ?>
 
   <!--  -->
@@ -222,22 +202,8 @@
 
 <div id="event_banner">
 		<div class="row mt mb">
-			<div class="col-xs-12 welcome_txt text-center">
-				<h1 class="event_banner_title">
-					Dance Marathon 2019
-					</h1>
-			</div>
-			<div class="col-xs-12 welcome_txt text-center">
-				<h1 class="event_banner_title">
-						<span style="color: #F79B63; background-color: white;">Is Here</span>
-					</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4 text-center">
-				<a id="event_page_button" class="btn hvr-underline-from-center" href="/dance-marathon">View Livestream</a>
-			</div>
-		</div>
+    </div>
+			
 	</div>
 
 
