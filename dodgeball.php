be62ac317928b2a587b499671f7797e7ea6e3e74
Unknown <ctcook97@gmail.com>
2019-10-03 12:06:20 -0400

    Width/height

--- dodgeball.php
+++ dodgeball.php
@@ -5,7 +5,7 @@
 ?>
 
 <div>
-<iframe src="https://challonge.com/amqxodwn/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+<iframe src="https://challonge.com/amqxodwn/module" width="100%" height="100%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
 </div>
 
 <?php ?>
