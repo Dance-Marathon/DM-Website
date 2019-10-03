a315c3102f066625596d84660fef9333fe4f400c
Unknown <ctcook97@gmail.com>
2019-10-03 12:02:58 -0400

    Get rid of banner/footer

--- dodgeball.php
+++ dodgeball.php
@@ -2,12 +2,10 @@
 	$status = "open";
 	$GLOBALS['page_title'] = 'Dodgeball Bracket';
 	$GLOBALS['parent'] = 'events';
-	include("includes/head.php");
-	include("includes/navbar.php");
 ?>
 
 <div>
 <iframe src="https://challonge.com/amqxodwn/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
 </div>
 
-<?php include("includes/foot.php"); ?>
+<?php ?>
