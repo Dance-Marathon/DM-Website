c0bfcec5a9c40afb5d1bf16f1a8cd33f0538403d
Kyle Bassignani <kylewbassignani@ufl.edu>
2020-05-11 19:19:28 -0400

    youtube video

--- mini-marathon.php
+++ mini-marathon.php
@@ -173,7 +173,7 @@
           <div class="tab-content">
             <div role="tabpanel" class="tab-pane fade in active" id="mini-marathon">
             <p>24 high schools around the state of Florida participate each year in our Mini Marathon program. High school students at these schools show an enthusiastic passion for Dance Marathon even before their college years begin and play a vital role in our movement. This past year, our Mini Marathon programs raised a total of $626,314.23, proving that these students are truly one generation fighting for the next. All money raised by Mini Marathons goes toward DM at UF’s total amount raised, which directly benefits the children at UF Health Shands Children’s Hospital.</p>
-			<iframe width="560" height="315" src="https://www.youtube.com/watch?v=-qCnFuEPZOo&feature=youtu.be" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/-qCnFuEPZOo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <p>We are always looking for more high schools to join our Dance Marathon family. If you would like to learn more about our Mini-Marathon Programs or would like to get involved, please contact our Marathon Relations Overall Directors, Mady Benton and Alexis Pollak, at <a href="mbenton@floridadm.org">mbenton@floridadm.org</a> and <a href="apollak@floridadm.org">apollak@floridadm.org</a>.</p>
 
         
