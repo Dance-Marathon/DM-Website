1ab3227f41194d8c210840c12faed432da692d40
Kyle Bassignani <kylewbassignani@ufl.edu>
2020-09-10 20:44:53 -0400

    dma

--- alumni.php
+++ alumni.php
@@ -6,49 +6,49 @@
 	include("includes/navbar.php");
 	$overalls = array(
 		array(
-			'name'                  =>  'Kelsey Torgerson',
-			'current_city'          =>  'New York City - Manhattan',
-			'previous_involvement'  =>  'DM Dancer 2012</br>Community Events Captain 2012-2015</br>Family Relations Captain 2015-2017</br>Assistant Director of Family Events 2017</br>Jen Krug 2017</br>Miracle Network Distinguished Leadership Award 2017</br>DMA Member 2017-2019</p>',
-			'fun_fact'				=>  'I want to get an orange corgi and name her Cheddar.',
-			'favorite_memory'       =>  'At the FTKickball Tournament, Zander got hit in the face with the kickball and started to cry. His dad rushed over to him, but Zander asked for me to come comfort him. I wiped away his tears and told him that he was strong and tough. He went along playing all evening, and at the end of the event, because of how I interacted with their son, the Wyants asked me to become Zander\'s godmother. It is the best job in the world to love on my sweet Z baby!</p>',
-			'continued'             =>  'Living on Manhattan, I got connected to The Stand, New York City\'s Dance Marathon Program for young adults. I immediately joined the organization as the Family and Hospital Relations Director and fell in love with our Miracle Children at Cohen\'s Medical Center on Long Island. We fundraised over $80,000 in two years and were able to purchase incubator giraffe beds for the NICU. And now, I am happy to serve on the DMA Board for two years, giving back to the organization that gave me my world today.',
-			'image'                 =>  'KelseyT'
+			'name'                  =>  'Joey Whelihan',
+			'current_city'          =>  'Gainesville, FL',
+			'previous_involvement'  =>  'Recruitment Overall Director, Recruitment Captain, Dancer, Delegate',
+			'fun_fact'				=>  'I snuck into the Pit section at an Ariana Grande concert and caught the lollipop that she threw into the crowd',
+			'favorite_memory'       =>  'Ahhh there are so many. There are many memories that stick out in my mind, and all of them revolve around the many people that have influence my life as a part of this organization. The one that is sticking out to me right now is getting my Miracle Band cut off on stage during closing ceremonies right before we raise the total for DM 2017!',
+			'continued'             =>  'I try to remain FTK in everything that I do, and I will *officially* be a pediatrician in May 2021! I\'ve spent the last 4 years of my life learning medicine to be that physician that takes care of all of the kiddos that we benefit with the money that we raise. Every day in the hospital, I see the CMN balloons everywhere, and I remember the impact that we have on the care of these children and families. Realize that the Miracle Families only represent a small fraction of all of the patients that the money that you raise benefits!',
+			'image'                 =>  'Whelihan'
 		),
 		array(
-			'name'                  =>  'Mary Hanvey',
-			'current_city'          =>  'Boston, Massachusetts ',
-			'previous_involvement'  =>  'I was a 4 year dancer and a creative development captain for a year. </p>',
-			'fun_fact'              =>  'Even though I was a 4 year dancer, I think my dancing abilities actually got worse each year. ',
-			'favorite_memory'       =>  'There are so many amazing Dance Marathon memories, but I think my favorite is my first year as a dancer, I remember the reveal at the end of DM and realizing we raised over a million dollars! It was in that moment that it hit me just how important and amazing Dance Marathon really was and I fell in love with the cause. </p>',
-			'continued'             =>  'First and foremost, I’ve carried on Dance Marathon by joining the DMA to stay involved even from a thousand miles away! Also, I have continued to fundraise and to support my previous captain team members and organization’s dancers. In Boston, I enjoy talking about DM to those unfamiliar with the cause and wanting to learn more. Lastly, being a part of Dance Marathon at UF taught me so much about hard work, passion, and dedication and I try to incorporate those values into my life everyday. I love coming back every year for this great event and look forward to seeing what this year holds for DM! FTK always!',
-			'image'                 =>  'MaryH'
+			'name'                  =>  'Kelsey Torgerson',
+			'current_city'          =>  'Manhattan, New York City',
+			'previous_involvement'  =>  'Dancer (2012), Community Events Captain (2012-2015), Family Relations Captain (2015-2017), Family Relations AD of Outreach (2017), Jen Krug Award Recipient (2017), Miracle Network Dance Marathon Distinguished Leadership Award Recipient (2017), DMA General Member (2017-Present), DMA Board Member + Secretary (2019-2020), DMA Board President (2020-2021)',
+			'fun_fact'              =>  'Whenever I see someone sporting UF or Gator Sports merchandize around the City, I always say with great enthusiasm, "Go Gators!" and chomp with them! It truly is a Gator Nation, even in New York City!',
+			'favorite_memory'       =>  'My most favorite and cherished DM Memory is when Zander Wyant, a Miracle Child, became my Godson. I will forever be grateful that DM at UF brought my family and closest friends into my life!',
+			'continued'             =>  'To this day, having graduated Grad School from UF in 2017, I still live and breathe the mission of Dance Marathon in almost every, if not all, aspects of my life! To be For The Kids is a forever commitment. I am honored to serve as the President of the Dance Marathon Alumni for DM at UF 2021 and cannot wait to see the miracles we create together For All The Right Reasons! Go Gators.',
+			'image'                 =>  'Kelsey'
+		),
+		array(
+			'name'                  =>  'Alexandra ("Zandy") Hernandez',
+			'current_city'          =>  'Asheville',
+			'previous_involvement'  =>  'I served as a Recruitment Captain for a year, a Recruitment Assistant Director for a year, and then as the first Outreach Overall, starting the Miracle Gala and working with the DMA',
+			'fun_fact'              =>  'I ate pizza for every meal for a week because (1) I love pizza, and (2) it raised money for DM',
+			'favorite_memory'       =>  'Transform Today.  The energy on campus is unmatched and everyone comes together in such an incredible way.  Transform Today my senior year was unforgettable.  We set out to raise $262,000 and raised $422,557.  I will never forget the moment when our Finance Manager walked out and told us the total.  We were in awe, and I continue to be in awe of what DM at UF is capable of doing on Transform Today.',
+			'continued'             =>  'I carry on Dance Marathon by giving back every year, and by keeping it as a part of my conversations.  Dance Marathon defined my college experience, and whenever I have the opportunity to share the cause, I do.',
+			'image'                 =>  'Hernandez'
 		),
 		array(
 			'name'                  =>  'Maria Mandese',
 			'current_city'          =>  'Orlando, Florida',
-			'previous_involvement'  =>  '2013: Dancer</br>2014: Morale Captain</br>2015: Morale Captain + Line dance Team</br>2016: Morale Captain</br>2016-2019: DMA Member</p>',
+			'previous_involvement'  =>  '2013: Dancer</br>2014: Morale Captain</br>2015: Morale Captain + Line dance Team</br>2016: Morale Captain</br>2016-2020: DMA Member</p>',
 			'fun_fact'              =>  'When I was in middle school, my grandparents sent me to Space Camp at Kennedy Space Center! I also don’t know how to whistle!',
-			'favorite_memory'       =>  'WOW, so many incredible memories to choose from! I would have to say one being when I had the amazing privilege and honor to receive the Jen Krug Award my senior year. As it was announced, I felt my fellow morale captains and best friends surround me, hugging and holding me (since I literally almost fell to the ground). What a truly special and unforgettable moment as I walked to receive the award hugging some of the miracle children and their families along the way who have touched my life forever. This moment still brings tears to my eyes because I know that DM has made me who I am today, it has given me so much joy, hope and love. This award meant everything to me; it was for these miracle children who I will continue to fight for every day, for the millions of dollars raised, and for the passion I will always have for this amazing cause. ALSO can\'t forget, getting to dress up in crazy costumes while doing the line dance are some of my favorite memories as well. :)</p>',
-			'continued'             =>  '"Dance Marathon led me to my career as a Certified Child Life Specialist, where I help children and families cope with the psychosocial stressors and challenges of a child’s hospitalization. I continue to use my passion for Dance Marathon every day as a Child Life Specialist working at a Children’s Miracle Network hospital, Arnold Palmer Hospital for Children in Orlando, Florida (full circle right!?). I owe so much to Dance Marathon and have been back to Dance Marathon for the past 3 years since graduating. I will continue to go back every year and continue to fight, raise money and dance for these kiddos who deserve a chance to be a kid and have a future!',
-			'image'                 =>  'MaryM'
+			'favorite_memory'       =>  'WOW, so many incredible memories to choose from! I would have to say one being when I had the amazing privilege and honor to receive the Jen Krug Award my senior year. As it was announced, I felt my fellow morale captains and best friends surround me, hugging and holding me (since I literally almost fell to the ground). What a truly special and unforgettable moment as I walked to receive the award hugging some of the miracle children and their families along the way who have touched my life forever. This moment still brings tears to my eyes because I know that DM has made me who I am today, it has given me so much joy, hope and love. This award meant everything to me; it was for these miracle children who I will continue to fight for every day, for the millions of dollars raised, and for the passion I will always have for this amazing cause. ALSO can\'t forget, getting to dress up in crazy costumes while doing the line dance are some of my favorite memories as well. :)',
+			'continued'             =>  'Dance Marathon led me to my career as a Certified Child Life Specialist, where I help children and families cope with the psychosocial stressors and challenges of a child’s hospitalization. I continue to use my passion for Dance Marathon every day as a Child Life Specialist working at a Children’s Miracle Network hospital, Arnold Palmer Hospital for Children in Orlando, Florida (full circle right!?). I owe so much to Dance Marathon and have been back to Dance Marathon for the past 4 years since graduating (of course virtually this year!!). I will continue to go back every year and continue to fight, raise money and dance for these kiddos who deserve a chance to be a kid and have a future!',
+			'image'                 =>  'Mandese'
 		),
 		array(
-			'name'                  =>  'Joelle Waksman',
-			'current_city'          =>  'Atlanta, GA',
-			'previous_involvement'  =>  '2010: Dancer</br>2011: Morale Staff</br>2012: Morale Captain</br>2013: Morale Captain + Linedance Team</br>2014: Morale Overall</br>2014-2018: DMA Member </p>',
-			'fun_fact'              =>  'My first job was a "motivational dancer" at bar/bat mitzvahs (and the occasional quinceañera) for Pure Energy Entertainment.',
-			'favorite_memory'       =>  'I cannot even begin to try and pick one, at this exact moment the first one that came to mind was when we came up with the closing song for the linedance in 2013. We closed our eyes and listened to it and pictured those moments and I totally broke down, chills, crying, screaming. It was the best.</p>',
-			'continued'             =>  'Every year except for one, I have been back to DM at UF! It has been the absolute best to a part of the DMA whatever that has turned out to look like year after year. As I become more and more "irrelevant" to the current students involved, I feel even more connected to where this program has come from and where it is growing to. FTK!',
-			'image'                 =>  'joelle'
-		),
-		array(
-			'name'                  =>  'Taylor Chambers',
-			'current_city'          =>  'Gainesville, FL',
-			'previous_involvement'  =>  '2012: Staff (FTKrew)</br>2013: Dancer Relations Captain</br>2014: Dancer Relations Overall</br>2015: Overall Director</p>',
-			'fun_fact'              =>  'I was a competitive gymnast for 10 years',
-			'favorite_memory'       =>  'After a long and stressful day of exams DM had an ice cream hospitality night scheduled. I was exhausted but decided to stop in and grab a scoop before heading home. I ended up spending an hour with our Captain Teams Miracle Family, the Pences, laughing and making ridiculous videos. It completely washed away the stress of the day and has always stood out to me as one of the best nights where my eyes were opened to the magic of DM and what a special organization it is.</p>',
-			'continued'             =>  'Continuing to live in Gainesville and working for UF Health has given me the unique opportunity to maintain ties with our Miracle Families and DM at UF. I continue to support local events and work build relationships with the students each year. Whether it is simply writing an encouraging email or getting coffee, I like to be available and give any resources I can to continue to grow the cause we all know and love.',
-			'image'                 =>  'chambers'
+			'name'                  =>  'Mary Hanvey',
+			'current_city'          =>  'Boston, MA',
+			'previous_involvement'  =>  'I was a 4 year dancer and a creative development captain for a year.',
+			'fun_fact'              =>  'Even though I was a 4 year dancer, I think my dancing abilities actually got worse each year.',
+			'favorite_memory'       =>  'There are so many amazing Dance Marathon memories, but I think my favorite is my first year as a dancer, I remember the reveal at the end of DM and realizing we raised over a million dollars! It was in that moment that it hit me just how important and amazing Dance Marathon really was and I fell in love with the cause.',
+			'continued'             =>  'First and foremost, I’ve carried on Dance Marathon by joining the DMA to stay involved even from a thousand miles away! Also, I have continued to fundraise and to support my previous captain team members and organization’s dancers. In Boston, I enjoy talking about DM to those unfamiliar with the cause and wanting to learn more. Lastly, being a part of Dance Marathon at UF taught me so much about hard work, passion, and dedication and I try to incorporate those values into my life everyday. I\'ve loved watching DM grow and adapt each year, and I feel so blessed to have been able to been able to continue to participate, whether it\'s in the O Dome or virtually. FTK always!',
+			'image'                 =>  'Harvey'
 		)
 	);
 
