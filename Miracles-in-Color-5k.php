90f9dd621b2f5dd525efe2e804f3390fef4138af
haileynelson <55765426+haileynelson@users.noreply.github.com>
2021-01-10 20:18:43 -0500

    Update Miracles-in-Color-5k.php

--- Miracles-in-Color-5k.php
+++ Miracles-in-Color-5k.php
@@ -1,6 +1,6 @@
 <?php
 	$status = "open";
-	$GLOBALS['page_title'] = 'Miracle Gala | Events | Dance Marathon at UF';
+	$GLOBALS['page_title'] = 'Miracles in Color 5K | Events | Dance Marathon at UF';
 	$GLOBALS['parent'] = 'events';
 	include("includes/head.php");
 	include("includes/navbar.php");
@@ -39,8 +39,21 @@
 
 				<h2>Miracles in Color 5K</h2>
 
-                <p>Our annual Miracles in Color 5k is a color run. Runners and walkers become vibrant and colorful throughout the course as they pass by our many color stations. This event is great for all ages and athletic abilities and includes free food, a live DJ, and more! It will be held in February 2021, and registration for the event will open at the end of 2020.</p>
-				<p>For more information, please contact the Community Outreach Overall Director, Grace Crowder, at <a href="mailto:gcrowder@floridadm.org">gcrowder@floridadm.org</a>.</p>
+                <p>Welcome to the Miracles in Color 5K benefiting UF Health Shands Children's Hospital!
+Join us for the Miracles in Color 5K on February 13, 2021 at Flavet Field. Miracles in Color is open to anyone who wants to support our cause and have a great time. We couldn't think of a better way for you to spend your Saturday morning than with paint and by supporting a hospital that is so prominent in our community!
+  </p>
+                <p>The course runs through the University of Florida campus and will have color stations throughout the course as well as at the finish line. At each color station, runners will be doused in color.* All participants will be provided with a Miracles In Color 5K white shirt during registration.</p>
+				<p>This year we are offering a virtual 5K option! Virtual participants will be provided a “Race Day Bundle” with a t-shirt and colored powder. There are individual tickets, as well as tickets for groups of two or four. Virtual registration must be completed by February 4 to guarantee shipping arrival before February 13.</p>
+				<p>Registration closes Tuesday, Feb. 9 at 11:59 p.m. After that point, no more online registrations will be accepted. No registration will be allowed at the event due to capacity limits.</p>
+				<p>All proceeds will be donated to UF Health Shands Children's Hospital, our local Children's Miracle Network Hospital, from Dance Marathon at the University of Florida.</p>
+				<p>*Don't want to be colored with powder? We will give you something to indicate to our color team that you want to stay clean. </p>
+				<p>For more information, please contact the Operations Manager, Hannah Cooper, at <a href="mailto:hcooper@floridadm.org">hcooper@floridadm.org\</a>.</p>
+
+               <h2>Race Day Info</h2>
+
+			   <p>Please arrive 15 minutes before your scheduled run time to sign in. You will be sent a Safety Clearance and Liability Waiver Form on Friday, Feb. 12. Please fill this form out ahead of time and screenshot your results.</p>
+			   <p>There is parking available at Flavet Field, but please arrive early to ensure you have time to sign in before your scheduled race.</p>
+			   <p>Check your email for more information, and email hcooper@floridadm.org with any questions.</p>
 			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
 		</div>
