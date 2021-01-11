33b971b38c11ebe25c705bca48799a8c8d595e36
haileynelson <55765426+haileynelson@users.noreply.github.com>
2021-01-11 14:47:20 -0500

    faq for 5k update

--- Miracles-in-Color-5k.php
+++ Miracles-in-Color-5k.php
@@ -54,6 +54,37 @@ Join us for the Miracles in Color 5K on February 13, 2021 at Flavet Field. Mirac
 			   <p>Please arrive 15 minutes before your scheduled run time to sign in. You will be sent a Safety Clearance and Liability Waiver Form on Friday, Feb. 12. Please fill this form out ahead of time and screenshot your results.</p>
 			   <p>There is parking available at Flavet Field, but please arrive early to ensure you have time to sign in before your scheduled race.</p>
 			   <p>Check your email for more information, and email hcooper@floridadm.org with any questions.</p>
+
+			   <h2>FAQs</h2>
+			   <h3>What is a Color Run?</h3>
+			   <p>A Color Run is a 5 kilometer race involving powder paint, inspired by the Hindu Festival of Holi. Participants wear white at the beginning of the race and are covered with color by the end. To learn more about Holi you can visit <a href="https://www.holifestival.org/" target="_blank">here.</a> </p>
+              
+			   <h3>What safety precautions are being taken to prevent the spread and transmission of COVID-19?</h3>
+               <p>This plan has been developed within the current CDC, Florida Department of Health, and University of Florida guidelines for managing an event space while preventing the spread and transmission of COVID-19.</P>
+               <br>All UF students attending the Miracles in Color 5K will be required to provide proof of a “Cleared” status on their ONE.UF to indicate that they are cleared to attend campus events.</br>
+               <br>A safety clearance form will be sent to all participants prior to the event to screen for symptoms and exposure prior to attending the event. If participants are experiencing symptoms, exposed to COVID-19, or not cleared for campus, then they will not be permitted to participant in the event.</br>
+             
+			  <h3>Will I need to wear a facial covering while running?</h3>
+               <p>Yes, facial coverings are required for the duration of the event.</p>
+               <br>Masks with exhalation valves or vents allow air to be expelled and are prohibited because they do not provide protection to others. </br>
+                <br>In addition, neck gaiters and bandanas should not be worn unless no other face covering is available. </br>
+              
+			   <h3>How is social distancing being enforced?</h3>
+               <p>Each shift will be limited to 100 participants to keep capacities within University guidelines. Participants are invited to attend with up to three other members of their QuaranTeam and will be instructed to maintain social distancing. Dance Marathon at UF Captains will be at Flavet Field and on the course to remind participants of social distancing protocols.</p>
+              
+			  <h3>What is a QuaranTeam?</h3>
+              <p>A QuaranTeam is a group of individuals who choose to socialize together and follow a mutually agreed upon set of rules or guidelines during the pandemic.  More information can be found at https://gatorwell.ufsa.ufl.edu/wp-content/uploads/2020/08/FindingYourQuaranTeam.pdf. </p>
+
+			  <h3>What if I don’t want to be covered in colored powder?</h3>
+              <p>We will give you something to indicate to our color team that you want to stay clean.</p>
+
+			  <h3>Refund policy</h3>
+			  <p>If you are unable to attend the Miracles in Color 5K due to a positive COVID-19 test or exposure, please contact hcooper@floridadm.org.</p>
+
+              <h3>Will there be chip timing at the Miracles in Color 5K?</h3>
+			  <p>No, there is no official timing system for the 5K. </p>
+
+
 			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
 		</div>
