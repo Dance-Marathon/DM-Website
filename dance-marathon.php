86ca2d84e59522f93351f45692ce4e3acab549d7
madisonhersch <mhersch@floridadm.org>
2021-01-19 12:44:34 -0500

    Update dance-marathon.php

--- dance-marathon.php
+++ dance-marathon.php
@@ -47,7 +47,7 @@ include("includes/navbar.php");
 
 <p> We are so excited to see everyone safely at the O'Connell center on April 10 - 11, 2021! </p>
 <p>Dance Marathon at the University of Florida is excited to share information regarding our Main Event on April 10-11. Our main priority is keeping our members, Miracle Families and community safe while the COVID-19 pandemic continues to affect the world. We have been working with UF Screen, Test & Protect Program and the Stephen C. O’Connell Center to establish safe protocols and guidelines during our in-person event. </p>
-<p>We will offer a virtual event option that will provide participants with a complete Main Event experience. The virtual option will be open to anyone to accommodate varying levels of comfortability with their attendance to Dance Marathon 2021.</p>
+<p>We will also offer a virtual event option that will provide participants with a complete Main Event experience. The virtual option will be open to anyone to accommodate varying levels of comfortability with their attendance to Dance Marathon 2021.</p>
 <p>The Main Event will look different than it has the last 26 years, but we are excited for the opportunity to change and grow. After establishing safety protocols, our priority is to provide a memorable experience for all of our members while creating miracles for the children treated at UF Health Shands Children’s Hospital.</p>
 	<p>Follow us on social media for event updates! </p>
 
@@ -71,7 +71,7 @@ include("includes/navbar.php");
 				<br><br>On-site volunteer medical staff will be available to assist with any injuries or illnesses that may occur throughout the Main Event.
 				<br><br>The O’Connell Center and UF Screen, Test & Protect have adjusted capacities to allow for physical distancing. Capacities are broken down according to each location in the O’Connell Center and will be strictly enforced.
 				<br><br>All participants will be required to wear a face covering for the duration of the Main Event.
-				<br><br>Masks with exhalation valves or vents that allow air to be expelled are prohibited because they do not provide protection to others. 
+				<br><br>Masks with exhalation valves or vents that allow air to be expelled are prohibited because they do not provide protection to others.
 				<br><br>In addition, neck gaiters and bandanas should not be worn unless no other face covering is available.
 				<br><br>Participants will be asked to arrive with their own face covering, however, we will have disposable masks available if someone forgets their face covering.
 				<br><br>Visitors’ contact with UF students will be as limited as possible.
@@ -120,7 +120,7 @@ include("includes/navbar.php");
 			  Two shifts allow us to provide more Dancer spots while keeping our members safe and following the capacities from the O’Connell Center.
               </div>
             </div>
-          </div> 
+          </div>
           <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="headingFive">
               <h4 class="panel-title">
@@ -146,8 +146,8 @@ include("includes/navbar.php");
             </div>
             <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
               <div class="panel-body">
-			  Visitors must be invited by Dance Marathon at UF to attend the Main Event. More information will be provided soon regarding visitor invitations. 
-			  <br><br>Visitor invitations are in place to ensure the safety of our members and prevent the outside spread of COVID-19. 
+			  Visitors must be invited by Dance Marathon at UF to attend the Main Event. More information will be provided soon regarding visitor invitations.
+			  <br><br>Visitor invitations are in place to ensure the safety of our members and prevent the outside spread of COVID-19.
 			  <br><br>The Main Event will be live-streamed to provide a virtual opportunity for visitors to tune in to the event and stay up to date on what is happening live at the O’Connell Center.
               </div>
             </div>
@@ -162,8 +162,8 @@ include("includes/navbar.php");
             </div>
             <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
               <div class="panel-body">
-			  Dancers will preference shifts once their spot is confirmed. We cannot guarantee which shift a Dancer will be assigned due to space availability. Please see the Dancer FAQ section for more information on shifts, preference deadlines and Dancer groups. 
-			  <br><br>Captains will coordinate with their Overall Director to preference their assignments. Please see the Captain FAQ section for more information. 
+			  Dancers will preference shifts once their spot is confirmed. We cannot guarantee which shift a Dancer will be assigned due to space availability. Please see the Dancer FAQ section for more information on shifts, preference deadlines and Dancer groups.
+			  <br><br>Captains will coordinate with their Overall Director to preference their assignments. Please see the Captain FAQ section for more information.
 			  <br><br>Emerging Leaders will be assigned shifts based on their Unit. Please see the Emerging Leader FAQ section for more information.
               </div>
             </div>
@@ -192,7 +192,7 @@ include("includes/navbar.php");
             </div>
             <div id="collapseNine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingNine">
               <div class="panel-body">
-			  Captains, Emerging Leaders, Dancers and Ambassadors will be invited back to attend our Total Reveal in person. All members will be physically distanced in the 100 and 200 level stands for Total Reveal. The reveal will be live-streamed on various social media platforms for anyone to tune in. 
+			  Captains, Emerging Leaders, Dancers and Ambassadors will be invited back to attend our Total Reveal in person. All members will be physically distanced in the 100 and 200 level stands for Total Reveal. The reveal will be live-streamed on various social media platforms for anyone to tune in.
               </div>
             </div>
           </div>
@@ -299,11 +299,11 @@ include("includes/navbar.php");
             </div>
             <div id="collapseSixteen" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSixteen">
               <div class="panel-body">
-			  Dancer spots will be allocated to each Organization based on their yearlong fundraising and involvement. 
+			  Dancer spots will be allocated to each Organization based on their yearlong fundraising and involvement.
 			  <br><br>Organizations select Dancers from their Registered Dancer lists and submit the names to DM at UF. Organization Dancer lists will be due by March 26.
               </div>
             </div>
-          </div> 
+          </div>
 		</div>
 
 		<h4>Ambassadors</h4>
@@ -384,7 +384,7 @@ include("includes/navbar.php");
               </div>
             </div>
           </div>
-		 </div> 
+		 </div>
 
 		 <h4>Emerging Leaders Program</h4>
 		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
@@ -416,7 +416,7 @@ include("includes/navbar.php");
               </div>
             </div>
           </div>
-		 </div> 
+		 </div>
 
 		 <h4>High Schoolers</h4>
 		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
@@ -452,17 +452,17 @@ include("includes/navbar.php");
             <div class="panel-heading" role="tab" id="headingTwentysix">
               <h4 class="panel-title">
                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwentysix" aria-expanded="false" aria-controls="collapseTwentysix">
-				Will we need to find somewhere to stay if we attend in-person? 
+				Will we need to find somewhere to stay if we attend in-person?
                 </a>
               </h4>
             </div>
             <div id="collapseTwentysix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwentysix">
               <div class="panel-body">
-			  Dance Marathon at UF does not provide accommodations for visitors. 
+			  Dance Marathon at UF does not provide accommodations for visitors.
               </div>
             </div>
           </div>
-		</div> 
+		</div>
 
 		<br><h4>For additional questions, email mainevent@floridadm.org </h4>
 
