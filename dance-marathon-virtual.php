c94937b15bc04ac0ad680d3e273be00bf9ba3a6b
haileynelson <55765426+haileynelson@users.noreply.github.com>
2021-04-03 16:51:57 -0400

    adding new virtual event tab

---
+++ dance-marathon-virtual.php
@@ -0,0 +1,51 @@
+<?php
+	$status = "open";
+	$GLOBALS['page_title'] = 'Dance Marathon Virual Event 2021 | Events | Dance Marathon at UF';
+	$GLOBALS['parent'] = 'events';
+	include("includes/head.php");
+	include("includes/navbar.php");
+?>
+
+<div class="page-heading parallax gala">
+  <div class="inner-wrapper">
+	  <div class="container">
+    	<div class="row">
+	  		<div class="col-md-12">
+    			<h1>Dance Marathon Virtual Event 2021</h1>
+	  		</div>
+    	</div>
+	  </div>
+	</div>
+</div>
+
+<div class="page-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+  			<div class="sub-nav">
+          <ul>
+		  				<li><a href="/dance-marathon">Dance Marathon 2021</a></li>
+                        <li><a class="active">Dance Marathon Virtual Event 2021</a></li>
+						<li><a href="/mini-marathon">Mini-Marathon</a></li>
+						<li><a href="miracle-gala">Miracle Gala</a></li>
+						<li><a href="/Miracles-in-Color-5k">Miracles in Color 5K</a></li>
+						<li><a href="/Moralloween">Moralloween</a></li>
+						<li><a href="/Moral-Madness">Morale Madness</a></li>
+						<li><a href="/Transform-Today">Transform Today</a></li>
+          </ul>
+        </div>
+			</div>
+			<div class="col-md-8 col-md-push-1">
+      <?php if ($status == "open") { ?>
+
+				<h2>Dance Marathon Virtual Event</h2>
+
+<p>Information Update Soon to Come!!</p>
+
+			<?php } else { echo 'This page is currently closed.'; } ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include("includes/foot.php"); ?>
