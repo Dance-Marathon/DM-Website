358dfb6bfa076bda5c30d0b01335200605231d9e
haileynelson <55765426+haileynelson@users.noreply.github.com>
2021-04-04 11:47:54 -0400

    Update dance-marathon-virtual.php

--- dance-marathon-virtual.php
+++ dance-marathon-virtual.php
@@ -40,7 +40,270 @@
 
 				<h2>Dance Marathon Virtual Event</h2>
 
-<p>Information Update Soon to Come!!</p>
+                    <p>Dance Marathon at the University of Florida is hosting our Main Event on April 10-11. We are so excited to be able to have a portion of our members join us in the Stephen C. O’Connell Center and safely participate in two 13.1-hour shifts. We are also hosting a variety of virtual options throughout the Dance Marathon 2021 weekend for those unable to join us in person. Each tab has a schedule, as well as links for each group to follow to enjoy their virtual experience.</p>
+	<div role="tabpanel">
+
+<!-- Nav tabs -->
+         <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active"><a href="#dancers-virtual" aria-controls="dancers-virtual" role="tab" data-toggle="tab">Dancers</a></li>
+          <li role="presentation"><a href="#alumni-virutal" aria-controls="alumni-virtual" role="tab" data-toggle="tab">Alumni</a></li>
+          <li role="presentation"><a href="#general-virtual" aria-controls="general-virtual" role="tab" data-toggle="tab">General</a></li>
+         <!-- <li role="presentation"><a href="#dancing-as-an-individual" aria-controls="dancing-as-an-individual" role="tab" data-toggle="tab">Dancing as an Individual</a></li> -->
+ 
+         </ul>
+
+<!-- Tab panes -->
+         <div class="tab-content">
+          <div role="tabpanel" class="tab-pane fade in active" id="dancers-virtual">
+
+	          <h2>Dancers</h2>
+	          <p></p>
+	          <p>Dancers will have the opportunity to participate in Dance Marathon at UF 2021 in-person or virtually. In-person Dancers will attend the Orange Shift 1 on Saturday, April 10 from 8 a.m. to 9:06 p.m. or Blue Shift 2 on Sunday, April 11 from 6 a.m. to 7:06 p.m. We encourage all of our Dancers to continue their event online once their Dancer shift ends. Fully virtual Dancers have the option to join Theme Hour and Fundraising Zooms and tune into our live streams.</p>
+	          <p>On April 10 from 12-2 p.m., Dancers who are not participating in the in-person Orange Shift 1 are invited to join us on Norman Field for food, games and more. On April 11 from 2-4 p.m.,  Dancers who are not participating in the in-person Blue Shift 2 are invited to join us on Norman Field for food, games and more.</p>
+	          <p></p>
+			  <h3>Orange Shift Schedule</h3>
+			    <h4>Saturday</h4>
+	             <ul>
+				    <li>In event - 8:00 a.m. to 9:06 p.m.</li>
+				 </ul>
+				<h4>Sunday</h4>
+				 <ul>
+				    <li>Miracles on Broadway Zoom - 8:00 a.m. - 10:00 a.m.  Fundraising Breakouts</li>
+					<li>Dream for Donny - 10:30 - 11:00 p.m.</li>
+					<li>DM Games Zoom - 12:10 - 1:50 p.m.  Fundraising Berakouts</li>
+					<li>DM on a Field - Norman, 2:00 - 4:00 p.m.</li>
+					<li>SS DM Zoom - 3:10 - 5:00 p.m.  Fundraising Breakouts</li>
+					<li>Miracle Stories every hour (8:20 a.m., 9:20 a.m., 10:20 a.m., 11:20 a.m., 12:20 p.m., 1:20 p.m., 3:20 p.m., 4:20 p.m.)</li>
+					<li>Closing Ceremonies and Total Reveal: In the O’Dome 6:00 p.m. - 7:06 p.m. (Sign in at Gate 3 at the O’Dome by 5:45 p.m.)</li>
+			     </ul>
+
+			  <h3>Blue Shift Schedule</h3>
+				<h4>Saturday</h4>
+				 <ul>
+				    <li>Opening Ceremonies Live stream - 8:00 a.m. - 9:00 a.m.</li>
+					<li>Greece/Grease Zoom - 10:10 a.m. - 12:00 p.m.  Fundraising Breakout</li>
+					<li>DM on a Field - Norman, 12:00 - 2:00 p.m.</li>
+					<li>All for Arden - 12:30 - 1:00 p.m.</li>
+					<li>Survivor Zoom - 2:10 - 3:50 p.m.  Fundraising Breakout</li>
+					<li>Out of the Page Zoom - 5:10 - 7:00 p.m.  Fundraising Breakout</li>
+					<li>Miracle Stories every hour (10:20 a.m., 11:20 a.m., 12:20 a.m., 1:20 p.m., 2:20 p.m., 3:20 p.m, 5:20 p.m., 6:20 pm.)</li>
+			     </ul>
+				<h4>Sunday</h4>
+				 <ul>
+				    <li>In event - 6:00 a.m. - 7:06 p.m.</li>
+				 </ul>
+	            
+			 <h2>Virtual Dancer Schedule</h2>
+			   <h3>Saturday</h3>
+			   <ul>
+				    <li>Opening Ceremonies Live Stream - 8:00 a.m. - 9:00 a.m.</li>
+					<li>Theme Hour Zoom: Greece/Grease - 10:10 a.m. - 12:00 p.m.  Fundraising breakouts</li>
+					<li>DM on a Field - Norman, 12:00 - 2:00 p.m.</li>
+					<li>All for Arden - 12:30 - 1:00 p.m.</li>
+					<li>Theme Hour Zoom: Survivor - 2:10 - 3:50 p.m.  Fundraising breakouts</li>
+					<li>Theme Hour Zoom: Out of the Page - 5:10 - 7:00 p.m.  Fundraising breakouts</li>
+					<li>Miracle Stories every hour (10:20 a.m., 11:20 a.m., 12:20 a.m., 1:20 p.m., 2:20 p.m., 3:20 p.m, 5:20 p.m., 6:20 pm.)</li>
+			     </ul>
+			    <h3>Sunday</h3>
+				<ul>
+				    <li>Theme Hour Zooom: Miracles on Broadway Zoom - 8:00 a.m. - 10:00 a.m.  Fundraising Breakouts</li>
+					<li>Dream for Donny - 10:30 - 11:00 p.m.</li>
+					<li>Theme Hour Zoom: DM Games - 12:10 - 1:50 p.m.  Fundraising Berakouts</li>
+					<li>DM on a Field - Norman, 2:00 - 4:00 p.m.</li>
+					<li>Theme Hour Zoom: SS DM - 3:10 - 5:00 p.m.  Fundraising Breakouts</li>
+					<li>Miracle Stories every hour (8:20 a.m., 9:20 a.m., 10:20 a.m., 11:20 a.m., 12:20 p.m., 1:20 p.m., 3:20 p.m., 4:20 p.m.)</li>
+					<li>Closing Ceremonies and Total Reveal: In the O’Dome 6:00 p.m. - 7:06 p.m. (Sign in at Gate 3 at the O’Dome by 5:45 p.m.)</li>
+			     </ul>
+         </div>
+	
+         <div role="tabpanel" class="tab-pane fade in active" id="alumni-virtaul">
+
+	       <h2>Alumni</h2>
+	          <p></p>
+	          <p>Alumni will have the opportunity to participate in Dance Marathon 2021 through our virtual platforms. A schedule of events can be found on the “General” tab, and Alumni-specific virtual opportunities are available below. Alumni can participate in the Alumni Power Hour on Saturday, April 10 before attending the virtual Miracle Gala. Miracle Gala tickets can be purchased with the link below. All Alumni are invited to tune into our live streams with @dmatuf on Instagram and Dance Marathon at UF on Facebook for Miracle Stories, fundraising hours and more! </p>
+	          <p></p>
+	       <h3>Saturday</h3>
+			     <ul>
+				    <li>Opening Ceremonies Live Stream - 8:00 a.m. - 9:00 a.m.</li>
+					<li>Stop! It’s Alumni Time. Zoom: 3:30 - 4:30 p.m.
+                        <br>Stop! It’s Alumni Time kickoff Livestream 3:30 p.m.</br>
+                        <br>2020 Linedance Livestream: 4:20 p.m.</br>
+                     </li>
+					<li>Gala: 6:00 - 9:00 p.m.
+					    <br>6:00 - 7:00 p.m.: VIP Hour</br>
+                        <br>7:00 - 9:00 p.m.</br>
+                        <br>Purchase tickets here. The links will be emailed to you. Reach out to gcrowder@floridadm.org with any questions.</br>
+					</li>
+			    </ul>
+			    <h3>Sunday</h3>
+				<ul>
+				    <li>Opening Ceremonies Livestream: 6:00 a.m. - 7:00 a.m.</li>
+					<li>Alumni Zoom: 12:00 - 1:00 p.m.
+                        <br>2018 Linedance (4 year Dancer)</br>
+						<br>Livestream Watch Party: 12:10 p.m.</br>
+                    </li>
+					<li>Closing Ceremonies and Total Reveal Livestream: 6:10 p.m. - 7:06 p.m.</li>
+			     </ul>
+         </div>
+
+
+
+  <div role="tabpanel" class="tab-pane fade" id="general-vitual">
+	
+	<p>Information Update Coming Soon!</p>
+  </div>
+
+  <!--
+  <div role="tabpanel" class="tab-pane fade" id="dancer-faqs">
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q1">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="false" aria-controls="a1">
+			  If I register to dance, am I guaranteed a Dancer spot at DM?
+			</a>
+		  </h4>
+		</div>
+		<div id="a1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q1">
+		  <div class="panel-body">
+			Not necessarily. Dancer spots are allocated based off of participation and fundraising goals set for each Organization and individual.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q2">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a2" aria-expanded="false" aria-controls="a2">
+			  I already registered to fundraise, do I need to register to dance?
+			</a>
+		  </h4>
+		</div>
+		<div id="a2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q2">
+		  <div class="panel-body">
+			Yes, registering to fundraise and registering to dance are two separate things.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q3">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a3" aria-expanded="false" aria-controls="a3">
+			  Do I have to be a UF student to dance?
+			</a>
+		  </h4>
+		</div>
+		<div id="a3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q3">
+		  <div class="panel-body">
+		  Yes, you must be enrolled as a student during the Spring semester in which Dance Marathon takes place. This also applies to those registered with UF who are doing an internship in place of classes.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q4">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a4" aria-expanded="false" aria-controls="a4">
+			  I don’t have my DM shirt, can I still check in for spirit points?
+			</a>
+		  </h4>
+		</div>
+		<div id="a4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q4">
+		  <div class="panel-body">
+			You must be wearing a DM shirt to check in for spirit points. The purpose of wearing the shirt is to spread awareness around campus.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q5">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a5" aria-expanded="false" aria-controls="a5">
+			  How do I register to dance?
+			</a>
+		  </h4>
+		</div>
+		<div id="a5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q5">
+		  <div class="panel-body">
+			Click the Dancer Resources tab above for instructions.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q6">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a6" aria-expanded="false" aria-controls="a6">
+			  How do I register to fundraise?
+			</a>
+		  </h4>
+		</div>
+		<div id="a6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q6">
+		  <div class="panel-body">
+			See <a href="https://floridadm.org/register-to-fundraise">Register to Fundraise</a> for instructions.
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q7">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a7" aria-expanded="false" aria-controls="a7">
+			  Can I still dance if I am the Ambassador for my Organization’s team?
+			</a>
+		  </h4>
+		</div>
+		<div id="a7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q7">
+		  <div class="panel-body">
+			Yes!
+		  </div>
+		</div>
+	  </div>
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q9">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a9" aria-expanded="false" aria-controls="a9">
+			  Who can come to the Event?
+			</a>
+		  </h4>
+		</div>
+		<div id="a9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q9">
+		  <div class="panel-body">
+			Anyone!
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q13">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a13" aria-expanded="false" aria-controls="a13">
+			  What happens when someone registers to dance and isn’t given a spot?
+			</a>
+		  </h4>
+		</div>
+		<div id="a13" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q13">
+		  <div class="panel-body">
+			The money paid for the registration fee goes toward their Organization’s total amount raised.
+		  </div>
+		</div>
+	  </div>
+		
+	<div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="q14">
+		  <h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordion" href="#a14" aria-expanded="false" aria-controls="a13">
+			  What if I am not in an Organization and want to register as an individual?
+			</a>
+		  </h4>
+		</div>
+		<div id="a14" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q14">
+		  <div class="panel-body">
+			You do not have to be in an Organization to register to Dance. You can register as an individual using the link under Dancer Resources; individual Dancers will be placed onto the Gator Aides team. Gator Aides is a team that allows individual Dancers to participate in Dance Marathon at UF with a group although they are not registered with an Organization. 
+		  </div>
+		</div>
+	  </div>
+		
+	</div> -->
+  </div>
+</div>
+</div>
+
 
 			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
