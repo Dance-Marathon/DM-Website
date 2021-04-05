6a090072788297defbad0ec8a14d81a526ce5aa4
haileynelson <55765426+haileynelson@users.noreply.github.com>
2021-04-05 11:14:22 -0400

    Update dance-marathon-virtual.php

--- dance-marathon-virtual.php
+++ dance-marathon-virtual.php
@@ -23,7 +23,7 @@
 		<div class="row">
 			<div class="col-md-3">
   			<div class="sub-nav">
-          <ul>
+                     <ul>
 		  				<li><a href="/dance-marathon">Dance Marathon 2021</a></li>
                         <li><a class="active">Dance Marathon Virtual Event 2021</a></li>
 						<li><a href="/mini-marathon">Mini-Marathon</a></li>
@@ -32,8 +32,8 @@
 						<li><a href="/Moralloween">Moralloween</a></li>
 						<li><a href="/Moral-Madness">Morale Madness</a></li>
 						<li><a href="/Transform-Today">Transform Today</a></li>
-          </ul>
-        </div>
+                     </ul>
+            </div>
 			</div>
 			<div class="col-md-8 col-md-push-1">
       <?php if ($status == "open") { ?>
@@ -41,7 +41,7 @@
 				<h2>Dance Marathon Virtual Event</h2>
 
                     <p>Dance Marathon at the University of Florida is hosting our Main Event on April 10-11. We are so excited to be able to have a portion of our members join us in the Stephen C. O’Connell Center and safely participate in two 13.1-hour shifts. We are also hosting a variety of virtual options throughout the Dance Marathon 2021 weekend for those unable to join us in person. Each tab has a schedule, as well as links for each group to follow and enjoy their virtual experience.</p>
-	<div role="tabpanel">
+       <div role="tabpanel">
 
 <!-- Nav tabs -->
          <ul class="nav nav-tabs" role="tablist">
@@ -114,9 +114,9 @@
 					<li>Miracle Stories every hour (8:20 a.m., 9:20 a.m., 10:20 a.m., 11:20 a.m., 12:20 p.m., 1:20 p.m., 3:20 p.m., 4:20 p.m.)</li>
 					<li>Closing Ceremonies and Total Reveal: In the O’Dome 6:00 p.m. - 7:06 p.m. (Sign in at Gate 3 at the O’Dome by 5:45 p.m.)</li>
 			     </ul>
-         </div>
+          </div>
 	
-         <div role="tabpanel" class="tab-pane fade" id="alumni-virtaul">
+          <div role="tabpanel" class="tab-pane fade" id="alumni-virtaul">
 
 	       <h2>Alumni</h2>
 	          <p></p>
@@ -144,171 +144,25 @@
                     </li>
 					<li>Closing Ceremonies and Total Reveal Livestream: 6:10 p.m. - 7:06 p.m.</li>
 			     </ul>
-         </div>
+            </div>
 
 
 
-          <div role="tabpanel" class="tab-pane fade" id="general-vitual">
+            <div role="tabpanel" class="tab-pane fade" id="general-vitual">
 	
 	        <p>Information Update Coming Soon!</p>
-         </div>
+           </div>
 
-  <!--
-  <div role="tabpanel" class="tab-pane fade" id="dancer-faqs">
-	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q1">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="false" aria-controls="a1">
-			  If I register to dance, am I guaranteed a Dancer spot at DM?
-			</a>
-		  </h4>
-		</div>
-		<div id="a1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q1">
-		  <div class="panel-body">
-			Not necessarily. Dancer spots are allocated based off of participation and fundraising goals set for each Organization and individual.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q2">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a2" aria-expanded="false" aria-controls="a2">
-			  I already registered to fundraise, do I need to register to dance?
-			</a>
-		  </h4>
-		</div>
-		<div id="a2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q2">
-		  <div class="panel-body">
-			Yes, registering to fundraise and registering to dance are two separate things.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q3">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a3" aria-expanded="false" aria-controls="a3">
-			  Do I have to be a UF student to dance?
-			</a>
-		  </h4>
-		</div>
-		<div id="a3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q3">
-		  <div class="panel-body">
-		  Yes, you must be enrolled as a student during the Spring semester in which Dance Marathon takes place. This also applies to those registered with UF who are doing an internship in place of classes.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q4">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a4" aria-expanded="false" aria-controls="a4">
-			  I don’t have my DM shirt, can I still check in for spirit points?
-			</a>
-		  </h4>
-		</div>
-		<div id="a4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q4">
-		  <div class="panel-body">
-			You must be wearing a DM shirt to check in for spirit points. The purpose of wearing the shirt is to spread awareness around campus.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q5">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a5" aria-expanded="false" aria-controls="a5">
-			  How do I register to dance?
-			</a>
-		  </h4>
-		</div>
-		<div id="a5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q5">
-		  <div class="panel-body">
-			Click the Dancer Resources tab above for instructions.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q6">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a6" aria-expanded="false" aria-controls="a6">
-			  How do I register to fundraise?
-			</a>
-		  </h4>
-		</div>
-		<div id="a6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q6">
-		  <div class="panel-body">
-			See <a href="https://floridadm.org/register-to-fundraise">Register to Fundraise</a> for instructions.
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q7">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a7" aria-expanded="false" aria-controls="a7">
-			  Can I still dance if I am the Ambassador for my Organization’s team?
-			</a>
-		  </h4>
-		</div>
-		<div id="a7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q7">
-		  <div class="panel-body">
-			Yes!
-		  </div>
-		</div>
-	  </div>
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q9">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a9" aria-expanded="false" aria-controls="a9">
-			  Who can come to the Event?
-			</a>
-		  </h4>
-		</div>
-		<div id="a9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q9">
-		  <div class="panel-body">
-			Anyone!
-		  </div>
-		</div>
-	  </div>
+  
+		
+	   </div>
 
-	  <div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q13">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a13" aria-expanded="false" aria-controls="a13">
-			  What happens when someone registers to dance and isn’t given a spot?
-			</a>
-		  </h4>
-		</div>
-		<div id="a13" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q13">
-		  <div class="panel-body">
-			The money paid for the registration fee goes toward their Organization’s total amount raised.
-		  </div>
-		</div>
-	  </div>
-		
-	<div class="panel panel-default">
-		<div class="panel-heading" role="tab" id="q14">
-		  <h4 class="panel-title">
-			<a data-toggle="collapse" data-parent="#accordion" href="#a14" aria-expanded="false" aria-controls="a13">
-			  What if I am not in an Organization and want to register as an individual?
-			</a>
-		  </h4>
-		</div>
-		<div id="a14" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q14">
-		  <div class="panel-body">
-			You do not have to be in an Organization to register to Dance. You can register as an individual using the link under Dancer Resources; individual Dancers will be placed onto the Gator Aides team. Gator Aides is a team that allows individual Dancers to participate in Dance Marathon at UF with a group although they are not registered with an Organization. 
-		  </div>
-		</div>
-	  </div>
-		
-	</div> -->
-          </div>
-        </div>
-    </div>
 
 
 			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
 		</div>
 	</div>
-</div>
+ </div>
 
 <?php include("includes/foot.php"); ?>
