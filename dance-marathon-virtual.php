15bcd82f9119e5ae3096a88474f1523cb5146b7f
brianna-barron <71742099+brianna-barron@users.noreply.github.com>
2021-06-07 11:23:20 -0400

    Update dance-marathon-virtual.php

--- dance-marathon-virtual.php
+++ dance-marathon-virtual.php
@@ -5,7 +5,11 @@
 	include("includes/head.php");
 	include("includes/navbar.php");
 ?>
-
+<style>
+.page-heading.parallax.gala {
+background-image: url("/assets/images/BannerPhotos21/erica_marjorie-116.jpg");
+}
+</style>
 <div class="page-heading parallax gala">
   <div class="inner-wrapper">
 	  <div class="container">
