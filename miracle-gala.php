9a4513924475ad43e83a856458a9a0cd58576b81
haileyjpaek <91424916+haileyjpaek@users.noreply.github.com>
2021-10-11 11:45:44 -0400

    Update miracle-gala.php

--- miracle-gala.php
+++ miracle-gala.php
@@ -51,7 +51,6 @@ background-image: url("/assets/images/BannerPhotos21/Overnight_SarahD30.jpg");
 <ul>
 <li>$40 for a VIP ticket</li>
 <ul>
-<li>Purchase your VIP event tickets by the evening of 3/28 to ensure you get your event package on time</li>
 <li>Includes an Event Package shipped to you with exclusive merchandise, Miracle Gala items, and more </li>
 <li>VIP Hour before the event</li>
 </ul>
