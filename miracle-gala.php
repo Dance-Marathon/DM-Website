780f22db40f4e86831dfe55ea5056ece539ec3ce
haileyjpaek <paekhailey@ufl.edu>
2021-10-12 22:26:15 -0400

    Update miracle-gala.php

--- miracle-gala.php
+++ miracle-gala.php
@@ -44,21 +44,19 @@ background-image: url("/assets/images/BannerPhotos21/Overnight_SarahD30.jpg");
 
 				<h2>Miracle Gala</h2>
 
-<p>Tickets for the 2021 virtual Dance Marathon Miracle Gala are no longer available. </p>
-<p>Each year, we invite guests to join us for an evening of fine dining and dancing to recognize all of the accomplishments of Dance Marathon at UF and the doctors at UF Health Shands Children’s Hospital. This year, the Miracle Gala will be held 100% virtually. </p>
-<p>We are focusing on reminiscing on and celebrating the past 27 years of DM at UF, with the theme being “Our Time Then vs. Our Time Now.” The night will be filled with many Special Guests, including Miracle Families, UF Health Shands Staff, members of our High School Program, Keynote Speakers, an Alumni Spotlight, and more! All attendees will also have access to participate in a Silent Auction and Raise the Paddle.</p>
+<p>Each year, we invite guests to join us for an evening of fine dining and dancing to recognize all of the accomplishments of Dance Marathon at UF and the doctors at UF Health Shands Children’s Hospital. </p>
+<p>We are focusing on reminiscing on and celebrating the past 28 years of DM at UF, with the theme being “Our Time Then vs. Our Time Now.” The night will be filled with many Special Guests, including Miracle Families, UF Health Shands Staff, members of our High School Program, Keynote Speakers, an Alumni Spotlight, and more! All attendees will also have access to participate in a Silent Auction and Raise the Paddle.</p>
 <p>There are two ticket options available:</p>  
 <ul>
 <li>$40 for a VIP ticket</li>
 <ul>
-<li>Purchase your VIP event tickets by the evening of 3/28 to ensure you get your event package on time</li>
 <li>Includes an Event Package shipped to you with exclusive merchandise, Miracle Gala items, and more </li>
 <li>VIP Hour before the event</li>
 </ul>
 <li>$10 for a General Ticket </li>
 <li>Legacy DMA Members will be offered a 20% discount on either option.</li>
 </ul>    
-<p>Although the event will look a little different, we still encourage you to put on your Black-tie attire and get ready to enjoy a night to remember, as the Miracle Gala is a beautiful night to celebrate our movement and the incredible strides we make each year!</p> 
+<p>We encourage you to put on your Black-tie attire and get ready to enjoy a night to remember, as the Miracle Gala is a beautiful night to celebrate our movement and the incredible strides we make each year!</p>
 <p>For more information, please contact the Community Outreach Overall Director, Melissa Tempest, at <a href=mailto:mtempest@floridadm.org>mtempest@floridadm.org</a>. We look forward to seeing you there!</p>       
 			<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
