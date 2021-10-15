5b7e7dd6d19392d02a543adf3cacf7272f33c714
brianna-barron <71742099+brianna-barron@users.noreply.github.com>
2021-10-15 14:05:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       </ol> -->
       <div class="carousel-inner">
         <div class="item active">
-          <img class="img-responsive"  src="assets/images/sectionbg/2021/TotalRevealhome.jpeg">
+          <img class="img-responsive"  src="assets/images/sectionbg/2021/TotalReveal.jpeg">
           <div class="carousel-caption text-center">
             <div class="row animate_fade_in">
               <!-- <div class="col-md-6">
