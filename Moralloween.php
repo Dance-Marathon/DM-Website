b6b523a07808a80b8d266d8b19ce5e58bf4eaa1b
brianna-barron <71742099+brianna-barron@users.noreply.github.com>
2021-11-17 09:36:37 -0500

    Update Moralloween.php

--- Moralloween.php
+++ Moralloween.php
@@ -29,7 +29,6 @@ background-image: url("/assets/images/BannerPhotos21/Moralloween_SXD101.jpg");
   			<div class="sub-nav">
           <ul>
 		  				<li><a href="/dance-marathon">Dance Marathon 2021</a></li>
-					    <li><a href="/dance-marathon-virtual">Dance Marathon Virtual Event 2021</a></li>
 						<li><a href="/mini-marathon">Mini-Marathon</a></li>
 						<li><a href="/miracle-gala">Miracle Gala</a></li>
 						<li><a href="/Miracles-in-Color-5k">Miracles in Color 5K</a></li>
