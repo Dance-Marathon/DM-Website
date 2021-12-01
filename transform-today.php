4ab8c33e426964b846701275cc9a091f8fd63d4d
brianna-barron <71742099+brianna-barron@users.noreply.github.com>
2021-12-01 00:36:46 -0500

    Update transform-today.php

--- transform-today.php
+++ transform-today.php
@@ -42,13 +42,21 @@ background-image: url("/assets/images/BannerPhotos21/KVO-1091.jpg");
       <?php if ($status == "open") { ?>
 
 				<h2>Transform Today</h2>
+                              Transform Today is our largest fall fundraising effort which lasts 26.2 hours and took place on October 26th-27th this year! Our movement spread throughout our organization, campus and community as our different teams’ colored polos flooded the University of Florida; our participants took the initiative to dedicate their day to raise funds and awareness for patients at UF Health Shands Children’s Hospital. <br></br>
+
+                              In efforts to boost engagement and encourage fundraising, we hosted programming at the Reitz Student Union in the Grand Ballroom and North Lawn. Throughout the day, merchandise, fundraising activities and Miracle Stories engaged and connected our members to our cause. We would like to extend a huge thank you to our participants, Alumni and donors for your continuous support throughout the day!<br></br>
+
+                              Our overall total, which was raised in just 26.2 hours for our hospital, was <b>$589,484.19! </b> <br></br>
+
+                              We are so eager to continue our efforts throughout this year and make miracles for the families who need it most! Please feel free to watch our Transform Today recap video <a href="https://www.youtube.com/watch?v=re8WypH-GlI&list=PLifN5hH-S4iQe14u-fHpP6cafBRGGPURj&index=7" target"_blank">here</a>.
+<br></br>
                 <h4><i>Why</i></h4>
 
                 For 26.2 hours, Dance Marathon at the University of Florida unites during this fundraising campaign to transform the lives of the patients at UF Health Shands Children’s hospital. In only 26.2 hours, our campus community rises to create as many miracles as possible for the children and families at Shands Children’s Hospital.
 <br></br>
 <h4><i>When</i></h2>
 
-Transform Today 2022 is from 9:00 a.m. on October 26th until 11:12 a.m. on October 27th!  </li>
+Transform Today 2022 was from 9:00 a.m. on October 26th until 11:12 a.m. on October 27th!  </li>
 <br></br>
 <h4><i>What</i></h4>
 
