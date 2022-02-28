002178382e12c90d5da8e474c00c70caaaa06f2f
kshamashetty <42147569+kshamashetty@users.noreply.github.com>
2022-02-27 21:28:52 -0500

    Update about.php

--- about.php
+++ about.php
@@ -11,6 +11,7 @@ background-image: url("/assets/images/BannerPhotos21/Charles_H-14.jpg");
 }
 </style>
 
+<!--Image Header-->
 <div class="page-heading parallax about">
   <div class="inner-wrapper">
 	  <div class="container">
@@ -23,6 +24,7 @@ background-image: url("/assets/images/BannerPhotos21/Charles_H-14.jpg");
 	</div>
 </div>
 
+<!--Text-->
 <div class="page-content">
 	<div class="container">
 		<div class="col-md-8 col-md-push-1">
@@ -31,6 +33,8 @@ background-image: url("/assets/images/BannerPhotos21/Charles_H-14.jpg");
 				<p>Out of 150,000 yearly patients, UF Health Shands Children’s Hospital sees more than 8,500 pediatric cases each year. <b style="color:#014083;">All of the funds raised by DM at UF go directly to purchasing state-of-the-art medical equipment, enhancements to make the children’s stay at the hospital more enjoyable, and funding for research grants and educational programs</b>.
 				<p>Patient care funding provides necessities such as bandages and patient gowns, as well as equipment and hospital improvements to make hospital stays more enjoyable for children and their families. Research funding helps our doctors find solutions for incurable diseases such as cystic fibrosis and cancer, and it allows them to develop life-saving technology and enhanced processes that improve the daily lives of pediatric patients. Education funding supports programs such as Child Life Diversion therapy and goes toward raising funds and awareness for the Children’s Miracle Network.
 				<p> <b style="color:#014083;">As we head into our </b> <b style="color:#F79B63;">28th year</b><b style="color:#014083;">, Dance Marathon at UF is excited to make even more miracles!</b></p>
+
+				<p><a class="btn btn-default" href="blog" role="button">Check Out Our Blog</a></p>
 
 				<?php } else { echo 'This page is currently closed.'; } ?>
 			</div>
