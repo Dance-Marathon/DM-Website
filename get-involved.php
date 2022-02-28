92b2fc549d6d12ed5b8f5851e39a35412cd4b1a7
victoriadealba <victoriadealba@ufl.edu>
2022-02-28 12:17:03 -0500

    Update with clickable tiles

--- get-involved.php
+++ get-involved.php
@@ -4,7 +4,59 @@
 	$GLOBALS['parent'] = 'get-involved';
 	include("includes/head.php");
 	include("includes/navbar.php");
+
+	$involvement = array (
+		array(
+			'name'=> 'Alumni',
+			'href'=>'alumni',
+			'image'=> 'Dina'
+		),
+		array (
+			'name'=> 'Ambassadors',
+			'href'=> 'ambassadors',
+			'image'=> 'About'
+		),
+		array (
+			'name'=> 'Captain Teams',
+			'href'=> 'captain-teams',
+			'image'=> 'Dina'
+		),
+		array(
+			'name'=> 'Dancers',
+			'href'=> 'dancers',
+			'image'=> 'About'
+		),
+		array (
+			'name'=> 'Emerging Leaders',
+			'href' => 'emerging-leaders',
+			'image'=> 'Dina'
+		),
+		array (
+			'name'=> 'Kirstyns Krew',
+			'href'=> 'kirstynskrew',
+			'image' => 'About'
+		),
+		array (
+			'name'=> 'Organizations',
+			'href'=> 'organizations',
+			'image'=> 'Dina'
+		),
+		array (
+			'name'=> 'Service',
+			'href'=> 'service',
+			'image'=> 'About'
+		)
+
+		);
+
+
+
+
+
+
 ?>
+
+
 <style>
 .page-heading.parallax.get-involved {
 background-image: url("/assets/images/BannerPhotos21/IMG_3231.jpg");
@@ -31,84 +83,92 @@ background-image: url("/assets/images/BannerPhotos21/IMG_3231.jpg");
 	</div>
 </div>
 
+
 <div class="page-content">
 	<div class="container">
 		<div class="row">
-		<div class="col-md-3">
-  			
-        </div>
+			<div class="col-md-3">
+				<div class="container text-center">
+					<p style="padding-top:0;">Take a look at all of the different ways <b style="color:#f79b63;"><b>you can get involved </b></b>with Dance Marathon at UF. There are so many ways to give back through DM at UF, and we would love for you to join our Dance Marathon family and <b style="color:#617ba6;"><b>help us make miracles.</b></b>
+       	   		 <p>
+			  	 <p><a class="btn btn-default" id="download-app" href="https://events.dancemarathon.com/index.cfm?fuseaction=donorDrive.event&eventID=4927" role="button">Register to Dance</a>
+	 				<p>
+
+				</div>
 			</div>
-			<div class="col-md-8 col-md-push-1">
-      <?php if ($status == "open") { ?>
-		<p style="padding-top:0;">Take a look at all of the different ways <b style="color:#f79b63;"><b>you can get involved </b></b>with Dance Marathon at UF. There are so many ways to give back through DM at UF, and we would love for you to join our Dance Marathon family and <b style="color:#617ba6;"><b>help us make miracles.</b></b>
- <p>
-	<p><a class="btn btn-default" id="download-app" href="https://events.dancemarathon.com/index.cfm?fuseaction=donorDrive.event&eventID=4927" role="button">Register to Dance</a>
-	 <p>
 
- 
+			<div class = "col-md-12">
+			<div class="container text-center">
+			<h3> Opportunities </h3>
+				</div>	
+				<?php if ($status == "open") { ?>
 
-				<!-- <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-					<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="q1">
-							<h4 class="panel-title">
-								<a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="false" aria-controls="a1">Welcome</a>
-							</h4>
-						</div>
-						<div id="a1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="q1">
-							<div class="panel-body">
-								<h4>Letter from Drew/Joey/Rachel</h4>
-							 <table style="width:100%">
-									<tr>
-										<td></td>
-										<td></td>
-										<td></td>
-									</tr>
-								</table>
-							</div>
-						</div>
-					</div> -->
-				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-					<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="q1">
-							<h4 class="panel-title">
-								<a data-toggle="collapse" data-parent="#accordion" href="#a1" aria-expanded="true" aria-controls="a1"></a>
-							</h4>
-						</div>
-						<div id="a1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="q1">
-							<div class="panel-body">
-								<h4>Register To Fundraise</h4>
-								Registering to fundraise is the first step to joining the Dance Marathon movement. Creating a fundraising page is quick and easy! Go to the Register to Fundraise page for instructions on how to sign up.
+				<?php
+        for($i = 0; $i < count($involvement); $i++) {
+        	 if($i % 4 == 0) {
+            echo '<div class="row">';
+         	 } ?>
+			
+			<div class="col-xs-3">
+			<!---Button trigger modal --> 
+			<a href="<?php echo ('/'.$involvement[$i]['href']); ?>"  class="btn btn-active" data-toggle="modal" >
+                    <img class="img-square img-thumbnail" src="<?php echo ('assets/images/atest/'.$involvement[$i]['image']); ?>.jpg" alt="..."><br>
+                    <p><?php echo $involvement[$i]['name']; ?></p>
+                </a>
+			</div>
+
+				<?php
+          		if(($i - 3) % 4 == 0) {
+           			echo '</div>';
+          		}
+				} ?>		
+				<?php } else { echo 'This page is currently closed.'; } ?>
 
 
-								<h4>Get Involved with the Dance Marathon Family</h4>
-								<p>If you would like to get involved with our organization, check out the list of positions below!</p>
 
-									   <ul><a href="/organizations"><b>Organization:</b></a> 	Student Organizations work year-round to support our cause.</ul>
-                     <ul><a href="/dancers"><b>Dancer:</b></a> Dancers stand on their feet if able for the duration of our Main Event in April to raise funds and awareness for UF Health Shands Children’s Hospital.</ul>
-										 <ul><a href="/emerging-leaders"><b>Emerging Leader:</b> </a> Members of our Emerging Leaders Program work alongside the Recruitment Team, providing support to Captain Teams and brainstorming new ideas to ensure a successful future for our organization.</ul>
-                     <ul><a href="/captain-teams"><b>Captain:</b> </a> Captains belong to 1 of 16 teams and take on yearlong responsibilities specific to their respective teams, with the goal of supporting and promoting our organization.</ul>
-                     <ul><a href="/captain-teams"><b>Assistant Directors:</b> </a> Individuals who work all year with their respective Overall Directors to ensure steady growth and management within their Captain Teams.</ul>
-                     <ul><a href="/meet-the-overalls"><b>Overall Directors:</b> </a> The 21 individuals who lead Dance Marathon efforts throughout the year. They are responsible for leading their respective Captain Teams to oversee, expand, and promote our organization.</ul>
 
-							 <table style="width:100%">
-									<tr>
-										<td></td>
-										<td></td>
-										<td></td>
-									</tr>
-								</table>
-							</div>
-						</div>
-					</div>
+
+
+
 
 
              </div>
-						 <p> <b>Want to know more about DM?</b> Submit your information<a href="https://docs.google.com/forms/d/e/1FAIpQLSc1mXWGc9rfL4GL6375p7HDqFhBrqa_GNQENMN9gS-l4tCwdw/viewform"> here</a> and we will reach out to you!</p>
 
-			<?php } else { echo 'This page is currently closed.'; } ?>
-			</div>
-		</div>
+		</div>	
 	</div>
 </div>
+	
+		
+		
+		
+		
 
-<?php include("includes/foot.php"); ?>
+
+     
+	
+		  <!-- Modal -->
+		  <div class="modal fade" id="myModal<?php echo $i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h3 class="modal-title" id="#myModal<?php echo $i; ?>Label">Meet <?php echo $kids[$i]['name']; ?></h3>
+                  </div>
+                  <div class="modal-body">
+<!--
+                    <img src="<?php echo ('assets/images/atest/'.$kids[$i]['image']); ?>.png" class="img-responsive img-full" />
+                    <hr>
+-->
+                    <p>
+                        <?php echo $involvement[$i]['href']; ?>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+	
+			
+    
+
+
+
