3ba1e9b8950fa055b90dc168a5dc6d8ff2465000
haileyjpaek <91424916+haileyjpaek@users.noreply.github.com>
2022-05-15 14:16:10 -0400

    Update meet-the-kids.php

--- meet-the-kids.php
+++ meet-the-kids.php
@@ -271,7 +271,7 @@ background-image: url("/assets/images/BannerPhotos21/mtk02.jpeg");
       <?php if ($status == "open") { ?>
 
 				<h3>For The Families</h3>
-        <p>Dance Marathon at the University of Florida is very fortunate to have over 77 Miracle Families. Our Miracle Families are true inspirations and the pinnacle of what Dance Marathon stands for. The birthday parties, end-of-chemotherapy parties, and play dates that we have with the children constantly remind us of the true meaning behind “For All The Right Reasons.” The entire team at Dance Marathon is honored and privileged to have these amazing families attend Dance Marathon every year and support our students.</p>
+        <p>Dance Marathon at the University of Florida is very fortunate to have over 56 Miracle Families. Our Miracle Families are true inspirations and the pinnacle of what Dance Marathon stands for. The birthday parties, end-of-chemotherapy parties, and play dates that we have with the children constantly remind us of the true meaning behind “For All The Right Reasons.” The entire team at Dance Marathon is honored and privileged to have these amazing families attend Dance Marathon every year and support our students.</p>
         <p>If you have any questions about the Family Relations program, or would like to become a Dance Marathon Miracle Family, please contact the Family Relations Overall Director, Carley Smith, at <a href="mailto:csmith@floridadm.org">csmith@floridadm.org</a></p>
 
         <h3>Meet our Kids, Meet our Miracles</h3>
