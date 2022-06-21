d067369f020ff0870a6d1581eb46678353f9ae1c
haileyjpaek <91424916+haileyjpaek@users.noreply.github.com>
2022-06-21 15:59:58 -0400

    Update get-involved.php

--- get-involved.php
+++ get-involved.php
@@ -7,7 +7,7 @@
 ?>
 <style>
 .page-heading.parallax.get-involved {
-background-image: url("/assets/images/BannerPhotos21/Get involed.jpg");
+background-image: url("/assets/images/BannerPhotos21/Get involed (1).jpg");
 }
 </style>
 <div class="page-heading parallax get-involved">
