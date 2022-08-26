2fa6a58ca7ab4d6b0c1156d169bbb2da887f3add
brianna-barron <71742099+brianna-barron@users.noreply.github.com>
2022-08-26 09:54:32 -0400

    Update applications.php

--- applications.php
+++ applications.php
@@ -43,7 +43,7 @@ background-image: url("/assets/images/BannerPhotos21/DSC07473.jpg");
 			<div class="col-md-8 col-md-push-1">
       <?php if ($status == "open") { ?>
  					<h3>Applications </h3>
-					<br>Thank you for your interest in Dance Marathon at the University of Florida 2022.</br>
+					<br>Thank you for your interest in Dance Marathon at the University of Florida 2023.</br>
 					<br>The online application forms are linked below. In order to be considered for a a Captain, ELP, or Ambassador position, you must:,</br>
 					<ul>
 						<li>Complete the online application form</li>
@@ -55,19 +55,19 @@ background-image: url("/assets/images/BannerPhotos21/DSC07473.jpg");
 <br>
 </br>
                         
-<b>Manager Application</b> Now Closed</p>
-<!-- Click <a href="Documents/2022 Manager Application.pdf" target="_blank">here</a> to view the 2022 Manager Application</p>-->
+<!-- <b>Manager Application</b> Now Closed</p>
+Click <a href="Documents/2022 Manager Application.pdf" target="_blank">here</a> to view the 2022 Manager Application</p>
 <b>Overall Director Application</b> Now Closed </p>
-<!--Click <a href="Documents/2022 Overall Director Application.pdf" target="_blank">here</a> to view the 2022 Overall Director Application</p>-->
-					<!-- You can apply for both the Emerging Leaders Program and for Captain Teams. However, your Captain Application will take priority over your Emerging Leader Application. You can apply to be an Ambassador even if you hold another position, and you will be able to do both.</p> -->
-<b>Assistant Director Applications</b> Now Closed </p>
-<b>Captain Application</b> Now Closed </p>
-				<!--	The Captain Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_ey6G1kOFYWmcrTo" target="_blank">here</a>. We highly recommend completing your Application in a document and transferring it to Qualtrics when you’re ready to submit. This application will close at <b/>11:59 PM EST on Friday, September 3</b>.
-					<p>A PDF of all Captain Application questions can be found <a href="assets/PDFs/ApplicationQuestions.pdf" target="_blank">here</a>.</p>
--->
-<b>Emerging Leaders Application</b> Now Closed </p>
-				<!--	<p>The Emerging Leaders Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_0vzpwH8jOnh0YRw" target="_blank">here</a>. This application will close at <b>11:59 PM EST on Thursday, September 9</b>.</p> -->
-<b>Ambassador Application</b> Now Closed </p>
+Click <a href="Documents/2022 Overall Director Application.pdf" target="_blank">here</a> to view the 2022 Overall Director Application</p>
+					You can apply for both the Emerging Leaders Program and for Captain Teams. However, your Captain Application will take priority over your Emerging Leader Application. You can apply to be an Ambassador even if you hold another position, and you will be able to do both.</p>
+<b>Assistant Director Applications</b> Now Closed </p> -->
+<b>Captain Application</b> <!-- Now Closed --> </p>
+					The Captain Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_bDgyFaGUhIHfwDY" target="_blank">here</a>. We highly recommend completing your Application in a document and transferring it to Qualtrics when you’re ready to submit. This application will close at <b/>11:59 PM EST on Friday, September 2</b>.
+					<p>A PDF of all Captain Application questions can be found <a href="assets/PDFs/AppCaptainDescriptionsand.pdf" target="_blank">here</a>.</p>
+
+<b>Emerging Leaders Application</b> <!-- Now Closed </p> -->
+					<p>The Emerging Leaders Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_3TNT3WCljhDOJLw" target="_blank">here</a>. This application will close at <b>11:59 PM EST on Thursday, September 9</b>.</p>
+<b>Ambassador Application</b> Coming Soon! </p>
                         
                   <br></br>
                         <p>If you have any questions regarding applications, please reach out to our Operations Manager, Zach Zelenak, at <a href="mailto:zzelenak@floridadm.org">zzelenak@floridadm.org</a>.</p>
