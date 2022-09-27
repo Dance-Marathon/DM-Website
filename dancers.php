3d4a6e15bd03ebb3fbc323d4731825a4f4188faa
haileyjpaek <91424916+haileyjpaek@users.noreply.github.com>
2022-09-27 16:43:02 -0400

    Update dancers.php

--- dancers.php
+++ dancers.php
@@ -66,7 +66,7 @@ background-image: url("/assets/images/BannerPhotos21/Rachel_F-0710.jpg");
             <p>Dance Marathon at the University of Florida is an annual event benefiting the patients of UF Health Shands Children's Hospital in Gainesville, Florida. Each year, students stay awake and on their feet, if able, to raise money and awareness for Children's Miracle Network Hospitals.</p>
             <p>As a Dancer, you will experience one of UF’s most adored traditions. Throughout the duration of the Main Event, you will learn our Linedance, enjoy live entertainment, eat delicious food from our local Sponsors, and hear inspiring stories from our Miracle Families.</p>
             <p>In addition to standing, if able, for the duration of the Main Event, Dancers will have year-long activities to take part in such as a Dancer Retreat, Registered Dancer Workshops, bonding with their internal Organization and more!</p>
-            <p><a href="https://events.dancemarathon.com/index.cfm?fuseaction=donorDrive.event&eventID=4927" target="_blank">Click Here to Register to Dance</a></p>
+            <p><a href="https://events.dancemarathon.com/event/floridadm" target="_blank">Click Here to Register to Dance</a></p>
             <p>Please email Lily Batley, Dancer Relations Overall Director, at <a href="mailto:lbatley@floridadm.org">lbatley@floridadm.org </a> with any questions!</p>
           </div>
                 <br></br>
