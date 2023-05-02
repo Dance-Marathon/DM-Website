cca71d915c9dbda1be437f4c907adc34dee73986
CameronAppel <132320989+CameronAppel@users.noreply.github.com>
2023-05-01 23:10:20 -0400

    Update get-involved.php

--- get-involved.php
+++ get-involved.php
@@ -837,7 +837,9 @@ for($i = 0; $i < count($overalls); $i++) {
 <b>Overall Director Application</b> Now Closed </p>
 <!--Click <a href="Documents/2022 Overall Director Application.pdf" target="_blank">here</a> to view the 2022 Overall Director Application</p>-->
 <!-- You can apply for both the Emerging Leaders Program and for Captain Teams. However, your Captain Application will take priority over your Emerging Leader Application. You can apply to be an Ambassador even if you hold another position, and you will be able to do both.</p> -->
-<b>Assistant Director Applications</b> Now Closed </p>
+<b>Assistant Director Applications</b> <!--Now Closed </p>-->
+    The Assistant Director Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_ey6G1kOFYWmcrTo" target="_blank">here</a>. This application will close at <b/>5:00 PM EST on Wednesday, May 10</b>. 
+
 <b>Captain Application</b> Now Closed </p>
 <!--    The Captain Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_ey6G1kOFYWmcrTo" target="_blank">here</a>. We highly recommend completing your Application in a document and transferring it to Qualtrics when you’re ready to submit. This application will close at <b/>11:59 PM EST on Friday, September 3</b>.
 <p>A PDF of all Captain Application questions can be found <a href="assets/PDFs/ApplicationQuestions.pdf" target="_blank">here</a>.</p>
@@ -846,7 +848,7 @@ for($i = 0; $i < count($overalls); $i++) {
 <!--    <p>The Emerging Leaders Application can be found <a href="https://ufl.qualtrics.com/jfe/form/SV_0vzpwH8jOnh0YRw" target="_blank">here</a>. This application will close at <b>11:59 PM EST on Thursday, September 9</b>.</p> -->
 <b>Ambassador Application</b> Now Closed </p>
 <p></p>
-<p>If you have any questions regarding applications, please reach out to our Operations Manager, Zach Zelenak, at <a href="mailto:zzelenak@floridadm.org">zzelenak@floridadm.org</a>.</p>
+<p>If you have any questions regarding applications, please reach out to our Membership Manager, Toni JeBailey, at <a href="mailto:tjebailey@floridadm.org">tjebailey@floridadm.org</a>.</p>
                     
 </div>
 
