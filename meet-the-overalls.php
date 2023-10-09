02559b01ce3bb55cda1e7db41050815bfb9a352b
CameronAppel <cappel@floridadm.org>
2023-10-09 13:18:55 -0400

    Update OT Headshots

--- meet-the-overalls.php
+++ meet-the-overalls.php
@@ -330,7 +330,7 @@ background-image: url("/assets/images/BannerPhotos21/contact us copy.jpg");
           } ?>
           <div class="col-sm-4">
             <a href="#" style="display: block;" class="text-center" data-toggle="modal" data-target="#myModal<?php echo $i; ?>">
-              <img style="border: none;" class="img-circle img-thumbnail" src="<?php echo ('/assets/images/OT23 PreHeadshots/'.$overalls[$i]['image']); ?>" alt="..."><br>
+              <img style="border: none;" class="img-circle img-thumbnail" src="<?php echo ('/assets/images/OT24 Headshots/'.$overalls[$i]['image']); ?>" alt="..."><br>
               <h3><?php echo $overalls[$i]['name']; ?></h3>
             </a>
             <p style="text-align: center;">
