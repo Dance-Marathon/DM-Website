0540371c7102662a3d52c263500f31282b02c563
Eric Hengber <ehengber@gmail.com>
2024-06-17 22:46:44 -0400

    fundraising page link fixed

--- register-to-fundraise.php
+++ register-to-fundraise.php
@@ -213,19 +213,16 @@ function openCityAndSubTab(evt, cityName, storySub) {
      
                     <p>Just follow the steps below:</p>
                     <ol>
-                        <li>Follow this <a href="https://events.dancemarathon.com/index.cfm?fuseaction=donorDrive.event&eventID=5803" target="_blank">Link </a> to register now!
+                        <li>Follow this <a href="https://events.dancemarathon.com/index.cfm?fuseaction=donorDrive.event&eventID=6292" target="_blank">Link </a> to register now!
                             <li> Select <b>'Register Now' </b>
                                 <ul>
-                                      <li> If you have a DonorDrive account from DM at UF 2023, select <b>‘Login to your account’</b>. </li>
+                                      <li> If you have a DonorDrive account from DM at UF 2024, select <b>‘Login to your account’</b>. </li>
                                       <li> If you do not have a DonorDrive account, then create an account now.  </li>
                                     </ul> </ul>
                           <li> Next, select your role.
                                 <ul>
-                                       <li> If you are joining an Organization, click <b>‘Join a Team’</b> and then search for your team name. </li>
-                                            <ul>
-                                                <li> If your Organization isn’t there, no worries! Click <b>‘Create a Team’</b> by including your Team Name and Team Fundraising Goal. </li>
-                                            </ul> </ul>
-                          <li> After, you will finalize your registration by answering the questions presented.
+                                       <li> Select ‘Participant’ then ‘Individual’ or join the team of your choosing!
+                            <li> After, you will finalize your registration by answering the questions presented.
                             <li> Once you have registered on DonorDrive, you can customize your fundraising page by adding a profile picture, editing your story, and updating your fundraising goal. Use this <a href="assets/PDFs/how-to-guide.pdf" target="_blank">How-To Guide</a> for step by step instructions.
                             <li> From here, you can use the tools on DonorDrive to send emails from a template, track your fundraising, and share your fundraising link with others! </li>
                                 </ol>
